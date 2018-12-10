854d599c67d7387721144a580c0134e504fbc0a6
John Davies <daviesja@uk.ibm.com>
2018-12-10 16:34:46 +0000

    Choose PROCLIB dsn and member
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/zowe-copy-proc.sh
+++ scripts/zowe-copy-proc.sh
@@ -16,38 +16,52 @@
 echo "<zowe-copy-proc.sh>" >> $LOG_FILE
 
 exitone(){
+  tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
+  echo "Unable to create the PROCLIB member " >> $LOG_FILE
+  echo "Unable to create the PROCLIB member "   
+ 
+  echo "  "Failed to put ZOWESVR.JCL in a PROCLIB dataset.
+  echo "  "Please add it manually from $ZOWE_ROOT_DIR/ZOWESVR.JCL to your PROCLIB
+  echo "    ""To find PROCLIB datasets, issue /\$D PROCLIB in SDSF"
   echo "</zowe-copy-proc.sh>" >> $LOG_FILE
   exit 1
 }
 exitzero(){
+  tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
+  echo "  "PROC $membername placed in $proclib
+  echo "  PROC $membername placed in $proclib " >> $LOG_FILE
   echo "</zowe-copy-proc.sh>" >> $LOG_FILE
   exit 0
 }
 
-if [[ $# != 2 ]]
+if [[ $# != 3 ]]
 then
-	echo Usage: copyproc.sh ussfile membername
+	echo Usage: copyproc.sh ussfile membername proclib
 	echo ussfile: name of JCL file on USS
 	echo membername: name of member to be written to PROCLIB PDS
-  echo "Not called with two parameters for required ussfile , membername" >> $LOG_FILE
+  echo proclib: name of PROCLIB PDS, or "auto" for automatic selection
+  echo "Not called with 3 parameters: ussfile, membername, proclib" >> $LOG_FILE
+  echo "Called with $# parameters: $@" >> $LOG_FILE
   exitone
 fi
 
 ussfile=$1
-membername=$2
+membername=$2   # default is set in zowe-parse-yaml.sh
+proclib=$3      # default is set in zowe-parse-yaml.sh
 
 if [[ `basename $ussfile` == $ussfile ]] # add pathname if not present
 then
-	ussfile=`pwd`/$ussfile
+  ussfile=$INSTALL_DIR/files/templates/$ussfile
 fi
 
 # echo "Copying the file" $ussfile "to PROCLIB member" $membername
 ls $ussfile 1>/dev/null 2>/dev/null
 if [[ $? > 0 ]]
 then
-  echo Unable to list $ussfile
-  exit 1
+  echo Unable to list $ussfile >> $LOG_FILE
+  exitone 
 fi
+cp $ussfile $ZOWE_ROOT_DIR/ZOWESVR.JCL
 #echo "Preparing CLIST"
 
 # write user's env vars in log
@@ -61,7 +75,6 @@ echo logname=`logname` >> $LOG_FILE
 
 # obtain true TSO userid to create CLIST with
 userid=`tsocmd lu 2>/dev/null | sed -n 's/USER=\([^ ]*\) *NAME=.*/\1/p'`
-echo userid from LISTUSER=$userid >> $LOG_FILE
 
 # try to recover in the unlikely event that LISTUSER fails
 if [[ "$userid" = "" ]]
@@ -81,28 +94,42 @@ then
                 userid=IBMUSER  # default
             else
                 userid=$USER
-                echo userid obtained from USER >> $LOG_FILE
+                echo userid obtained from USER is $userid >> $LOG_FILE
             fi
         else
             userid=$LOGNAME
-            echo userid obtained from LOGNAME >> $LOG_FILE
+            echo userid obtained from LOGNAME  is $userid >> $LOG_FILE
         fi
     else
-        echo userid obtained from logname >> $LOG_FILE
+        echo userid obtained from logname  is $userid >> $LOG_FILE
     fi
 else
-    echo userid obtained from LISTUSER >> $LOG_FILE
+    echo userid obtained from LISTUSER  is $userid >> $LOG_FILE
 fi
 # end of recovery
 
 echo userid=\[$userid\] >> $LOG_FILE  # brackets to check for trailing blanks
 
-tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
+tsocmd delete "'$userid.zowetemp.clist' " 1> $TEMP_DIR/delete.clist.first 2>/dev/null
+if [[ $? -ne 0 ]]
+then 
+  grep -i "ENTRY $userid.zowetemp.clist NOT FOUND" $TEMP_DIR/delete.clist.first > /dev/null
+  if [[ $? -ne 0 ]]
+  then 
+    echo Failed to delete old dataset $userid.zowetemp.clist >> $LOG_FILE
+    cat $TEMP_DIR/delete.clist.first  >> $LOG_FILE    
+  else 
+    echo Old dataset $userid.zowetemp.clist did not exist >> $LOG_FILE
+  fi 
+else 
+  echo Old dataset $userid.zowetemp.clist deleted >> $LOG_FILE
+fi 
 
 tsocmd ALLOCATE "DATASET('$userid.zowetemp.clist') NEW SPACE(1) BLOCK(255) LRECL(255)  RECFM(F) DSORG(Po) dsntype(pds) dir(1)" 1>/dev/null 2>/dev/null
 if [[ $? > 0 ]]
 then
 	echo Failed to create new dataset $userid.zowetemp.clist >> $LOG_FILE
+  exitone 
 fi
 
 # put USS JCL file name in CLIST, because CLIST parms are always uppercased.
@@ -113,100 +140,104 @@ tsocmd oget "'$TEMP_DIR/ocopyshr.e.clist' '$userid.zowetemp.clist(copyproc)'"  1
 if [[ $? > 0 ]]
 then
 	echo Failed to put edited CLIST in $userid.zowetemp.clist >> $LOG_FILE
+  exitone
 fi
 #echo "CLIST copy done, RC $?"
 
-# echo Try USER.*.PROCLIB
-procs=`tsocmd listds \'user.\*.proclib\'` 1>/dev/null 2>/dev/null
-if [[ $? > 0 ]]
-then
-  echo "  "Unable to find any USER.*.PROCLIB >> $LOG_FILE
-else
-  procs=`tsocmd listds \'user.\*.proclib\' 2>/dev/null | grep USER\..*PROCLIB`
-  echo "    "procs = $procs >> $LOG_FILE
-  for proclib in $procs
-  do
-    echo "    "proclib = $proclib >> $LOG_FILE
-    ./ocopyshr.sh $proclib $membername
-    if [[ $? > 0 ]]
-    then
-      echo "  "Unable to write to $proclib, try another PROCLIB >> $LOG_FILE
-    else
-      echo "  "PROC $membername placed in $proclib
-      echo "  PROC $membername placed in $proclib " >> $LOG_FILE
-      tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
-      exitzero
-    fi
-  done
-fi
+# Process member name
+echo Member name $membername was specified >> $LOG_FILE
+# check membername is valid
+# Is member name too long?
+if [[ `echo ${membername} | wc -c` -gt 9 ]]        # 9 includes the string-terminating null character
+then 
+  echo Member name $membername longer than 8 characters
+  exitone
+  # echo Defaulting to ZOWESVR
+  # membername=ZOWESVR
+fi 
+# end of check
+echo Member name $membername was used >> $LOG_FILE
 
-# echo Try JES2 PROCLIB concatenation
-opercmd "d t" 1> /dev/null 2> /dev/null  # is 'opercmd' available?
-if [[ $? > 0 ]]
+# Process proclib name
+echo $proclib | grep -i ^auto$ 1>/dev/null 2>/dev/null  # allow for mixed case
+if [[ $? -ne 0 ]]
 then
-  echo "  "Unable to read JES2 PROCLIB concatenation with opercmd REXX exec >> $LOG_FILE
-else
-  procs=`opercmd '$d proclib'|grep DSNAME=.*\.PROCLIB|sed 's/.*DSNAME=\(.*\)\.PROCLIB.*/\1.PROCLIB/'`
-  echo "  "procs = $procs >> $LOG_FILE
-  for proclib in $procs
-  do
-    echo "  "proclib = $proclib >> $LOG_FILE
-    ./ocopyshr.sh $proclib $membername 
-    if [[ $? > 0 ]]
-    then
-      : # echo Unable to write to $proclib, try another PROCLIB
-    else
-      echo "  "PROC $membername placed in $proclib
-      echo "  PROC $membername placed in $proclib " >> $LOG_FILE
-      tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
-      exitzero
-    fi
-  done
-fi
-
-# echo Try master JES2 JCL
-tsocmd oput \'sys1.parmlib\(mstjcl00\)\' \'mstjcl00\' 1>/dev/null 2>/dev/null
-if [[ $? > 0 ]]
-then
-  : # echo Unable to read master JES2 JCL
-  rm mstjcl00
-else
-  procs=`grep PROCLIB mstjcl00 | sed 's/.*DSN=\(.*\)\.PROCLIB.*/\1.PROCLIB/'`
-  rm mstjcl00
-  for proclib in $procs
-  do
+  # auto was not requested
+  echo PROCLIB dataset \'$proclib\' was specified >> $LOG_FILE
+  # Try the requested PROCLIB
+  tsocmd listds \'$proclib\' 1>/dev/null 2>/dev/null
+  if [[ $? -ne 0 ]]
+  then
+    echo "  "Unable to list dataset $proclib >> $LOG_FILE
+    tsocmd listds \'$proclib\' 2>> $LOG_FILE 1>> $LOG_FILE # log the error
+    exitone 
+  else
+    echo "    "found PROCLIB dataset $proclib >> $LOG_FILE
       ./ocopyshr.sh $proclib $membername
       if [[ $? > 0 ]]
       then
-        : # echo Unable to write to $proclib, try another PROCLIB
+        echo "  "Unable to write to requested PROCLIB $proclib >> $LOG_FILE
+        exitone
       else
-        echo "  "PROC $membername placed in $proclib
-        echo "  PROC $membername placed in $proclib " >> LOG_FILE
-        tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
+        exitzero
+      fi
+    # done
+  fi
+
+else
+  echo \'auto\' was requested: choose PROCLIB dataset automatically >> $LOG_FILE
+  echo Try JES2 PROCLIB concatenation >> $LOG_FILE
+  opercmd "d t" 1> /dev/null 2> /dev/null  # is 'opercmd' available?
+  if [[ $? > 0 ]]
+  then
+    echo "  "Unable to read JES2 PROCLIB concatenation with opercmd REXX exec >> $LOG_FILE
+  else
+    procs=`opercmd '$d proclib'|grep DSNAME=.*\.PROCLIB|sed 's/.*DSNAME=\(.*\)\.PROCLIB.*/\1.PROCLIB/'`
+    echo "  "procs = $procs >> $LOG_FILE
+    for proclib in $procs
+    do
+      echo "  "proclib = $proclib >> $LOG_FILE
+      ./ocopyshr.sh $proclib $membername 
+      if [[ $? > 0 ]]
+      then
+        echo Unable to write to $proclib, try next PROCLIB >> $LOG_FILE
+      else
         exitzero
       fi
     done
   fi
 
-  # echo Try SYS1.PROCLIB
-  ./ocopyshr.sh $proclib $membername
+  echo Try master JES2 JCL >> $LOG_FILE
+  tsocmd oput \'sys1.parmlib\(mstjcl00\)\' \'mstjcl00\' 1>/dev/null 2>/dev/null
   if [[ $? > 0 ]]
   then
-    : # echo Unable to write to SYS1.PROCLIB
+    echo Unable to read master JES2 JCL >> $LOG_FILE
+    rm mstjcl00
   else
-    echo "  "PROC $membername placed in $proclib
-    echo "PROC $membername placed in $proclib " >> $LOG_FILE
-    tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
-    exitzero
-  fi
+    procs=`grep PROCLIB mstjcl00 | sed 's/.*DSN=\(.*\)\.PROCLIB.*/\1.PROCLIB/'`
+    rm mstjcl00
+    for proclib in $procs
+    do
+        ./ocopyshr.sh $proclib $membername
+        if [[ $? > 0 ]]
+        then
+          echo Unable to write to $proclib, try another PROCLIB >> $LOG_FILE
+        else
+          exitzero
+        fi
+      done
+    fi
 
-  tsocmd delete "'$userid.zowetemp.clist' " 1>/dev/null 2>/dev/null
+    echo Try SYS1.PROCLIB >> $LOG_FILE
+    proclib=SYS1.PROCLIB
+    ./ocopyshr.sh $proclib $membername
+    if [[ $? > 0 ]]
+    then
+      echo Unable to write to SYS1.PROCLIB >> $LOG_FILE
+    else
+      exitzero
+    fi
 
-  cp $ussfile $ZOWE_ROOT_DIR/ZOWESVR.JCL
- 
-  echo "  "Failed to put ZOWESVR.JCL in a PROCLIB dataset.
-  echo "  "Please add it manually from $ZOWE_ROOT_DIR/ZOWESVR.JCL to your PROCLIB
-  echo "    ""To find PROCLIB datasets, issue /\$D PROCLIB in SDSF"
-  echo "Unable to create the proclib " >> $LOG_FILE
+fi
 
 exitone
