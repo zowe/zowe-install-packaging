dfbbd0f6e82baedbb85dab983157f734c8c28112
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-01-14 10:49:28 -0500

    optimize explorer api/ui install/configure
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- scripts/zowe-explorer-ui-install.sh
+++ scripts/zowe-explorer-ui-install.sh
@@ -17,72 +17,29 @@
 # $LOG_FILE
 
 echo "<zowe-explorer-ui-install.sh>" >> $LOG_FILE
-cd $INSTALL_DIR
 
-#############################################
-# Install explorer-jes started
-EXPLORER_JES_PAX=$PWD/$(ls -t ./files/explorer-jes-*.pax | head -1)
-if [ ! -f $EXPLORER_JES_PAX ]; then
-  echo "JES Explorer UI (explorer-jes*.pax) missing"
-  echo "Installation terminated"
-  exit 0
-fi
-#############################################
-# Install explorer-mvs started
-EXPLORER_MVS_PAX=$PWD/$(ls -t ./files/explorer-mvs-*.pax | head -1)
-if [ ! -f $EXPLORER_MVS_PAX ]; then
-  echo "MVS Explorer UI (explorer-mvs*.pax) missing"
-  echo "Installation terminated"
-  exit 0
-fi
-#############################################
-# Install explorer-uss started
-EXPLORER_USS_PAX=$PWD/$(ls -t ./files/explorer-uss-*.pax | head -1)
-if [ ! -f $EXPLORER_USS_PAX ]; then
-  echo "USS Explorer UI (explorer-uss*.pax) missing"
-  echo "Installation terminated"
-  exit 0
-fi
+UI_PLUGIN_LIST="jes mvs uss"
+for one in $UI_PLUGIN_LIST; do
+  EXPLORER_PLUGIN_UPPERCASE=$(echo $one | tr '[a-z]' '[A-Z]')
 
-# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-EXPLORER_INSTALL_FOLDER=jes_explorer
-echo "Installing Explorer JES into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."
-echo "Installing Explorer JES into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
-umask 0002
-mkdir -p "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-# unpax package
-cd "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-echo "Unpax of ${EXPLORER_JES_PAX} into ${PWD}" >> $LOG_FILE
-pax -rf $EXPLORER_JES_PAX -ppx
-# Install explorer-jes ended
-#############################################
+  cd $INSTALL_DIR
+  EXPLORER_PLUGIN_PAX=$PWD/$(ls -t "./files/explorer-${one}-*.pax" | head -1)
+  if [ ! -f $EXPLORER_PLUGIN_PAX ]; then
+    echo "  ${EXPLORER_PLUGIN_UPPERCASE} Explorer UI (explorer-${one}-*.pax) missing"
+    echo "  Installation terminated"
+    exit 0
+  fi
 
-
-# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-EXPLORER_INSTALL_FOLDER=mvs_explorer
-echo "Installing Explorer MVS into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."
-echo "Installing Explorer MVS into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
-umask 0002
-mkdir -p "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-# unpax package
-cd "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-echo "Unpax of ${EXPLORER_MVS_PAX} into ${PWD}" >> $LOG_FILE
-pax -rf $EXPLORER_MVS_PAX -ppx
-# Install explorer-mvs ended
-#############################################
-
-
-# NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-EXPLORER_INSTALL_FOLDER=uss_explorer
-echo "Installing Explorer USS into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."
-echo "Installing Explorer USS into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
-umask 0002
-mkdir -p "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-# unpax package
-cd "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-echo "Unpax of ${EXPLORER_USS_PAX} into ${PWD}" >> $LOG_FILE
-pax -rf $EXPLORER_USS_PAX -ppx
-# Install explorer-uss ended
-#############################################
+  # NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
+  EXPLORER_INSTALL_FOLDER="${one}_explorer"
+  echo "  Installing Explorer UI ${EXPLORER_PLUGIN_UPPERCASE} into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."
+  echo "  Installing Explorer UI ${EXPLORER_PLUGIN_UPPERCASE} into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
+  umask 0002
+  mkdir -p "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
+  # unpax package
+  cd "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
+  echo "  Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
+  pax -rf $EXPLORER_PLUGIN_PAX -ppx
+done
 
 echo "</zowe-explorer-ui-install.sh>" >> $LOG_FILE
