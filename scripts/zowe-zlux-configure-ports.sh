6fcd39b58ff4da3dd5865ad7277e9e04023160f2
Petr Plavjanik <petr.plavjanik@broadcom.com>
2019-01-15 16:46:48 +0100

    Configure GW host in zLUX
    Signed-off-by: Petr Plavjanik <petr.plavjanik@broadcom.com>

--- scripts/zowe-zlux-configure-ports.sh
+++ scripts/zowe-zlux-configure-ports.sh
@@ -20,10 +20,11 @@ cd $ZLUX_SERVER_CONFIG_PATH/
 echo "Updating ports in zluxserver.json "$ZOWE_ZLUX_SERVER_HTTPS_PORT";"$ZOWE_ZSS_SERVER_PORT  >> $LOG_FILE 
 sed 's/"port": 8544,/"port": '"${ZOWE_ZLUX_SERVER_HTTPS_PORT}",'/g' zluxserver.json > $TEMP_DIR/transform1.json
 sed 's/"zssPort":8542/"zssPort": '"${ZOWE_ZSS_SERVER_PORT}"'/g' $TEMP_DIR/transform1.json > $TEMP_DIR/transform3.json
+sed 's/"hostname": "localhost",/"hostname": "'"${ZOWE_EXPLORER_HOST}"'",/g' $TEMP_DIR/transform3.json > $TEMP_DIR/transform4.json
 if grep -q gatewayPort "zluxserver.json"; then
-    sed 's/"gatewayPort":10010/"gatewayPort": '"${ZOWE_APIM_GATEWAY_PORT}"'/g' $TEMP_DIR/transform3.json > zluxserver.json
+    sed 's/"gatewayPort":10010/"gatewayPort": '"${ZOWE_APIM_GATEWAY_PORT}"'/g' $TEMP_DIR/transform4.json > zluxserver.json
 else
-    sed 's/"hostname"/"gatewayPort": '"${ZOWE_APIM_GATEWAY_PORT}"', "hostname"/g' $TEMP_DIR/transform3.json > zluxserver.json
+    sed 's/"hostname"/"gatewayPort": '"${ZOWE_APIM_GATEWAY_PORT}"', "hostname"/g' $TEMP_DIR/transform4.json > zluxserver.json
 fi
 
 # SSH port for the VT terminal app
