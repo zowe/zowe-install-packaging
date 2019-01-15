868b4c756c1922284107cfccb4a28d3a4f86dcac
Joe-Winchester <winchest@uk.ibm.com>
2019-01-15 14:20:48 +0000

    Remove explorer-server from zowe-stop.sh
    Signed-off-by: Joe-Winchester <winchest@uk.ibm.com>

--- scripts/zowe-stop.sh
+++ scripts/zowe-stop.sh
@@ -15,12 +15,6 @@ VAR=`dirname $0`			# Obtain the scripts directory name
 cd $VAR/..				# Change to its parent which should be ZOWE_ROOT_DIR
 ZOWE_ROOT_DIR=`pwd`			# Set our environment variable
 #echo ZOWE_ROOT_DIR = $ZOWE_ROOT_DIR
-if [[ ! -d $ZOWE_ROOT_DIR/explorer-server ]]
-then
-	echo This script must be in the ZOWE_ROOT_DIR/scripts directory
-	echo You are running from $VAR
-	exit 1
-fi
 echo Stopping ZOWE server
 #opercmd "P ZOWESVR.ZOWESVR"
 $ZOWE_ROOT_DIR/scripts/internal/opercmd "c ZOWESVR"
