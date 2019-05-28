bebe28fe1e9ada2abae7a48979be8080087e4887
Evghenii Vasilovici <evghenii.vasilovici@broadcom.com>
2019-05-28 09:25:11 +0200

    Add started task names to Zowe USS processes
    Signed-off-by: Evghenii Vasilovici <evghenii.vasilovici@broadcom.com>

--- scripts/zowe-parse-yaml.sh
+++ scripts/zowe-parse-yaml.sh
@@ -45,6 +45,12 @@ do
                 ZOWE_ROOT_DIR=`sh -c "echo $value"` 
                 export ZOWE_ROOT_DIR
             fi
+# Look for prefix= beneath install:
+            if [[ $key == "prefix" ]] && [[ $section == "install" ]]
+            then
+                ZOWE_PREFIX=$value
+                export ZOWE_PREFIX
+            fi
 # Look for jobsAPIPort= beneath zos-services:
             if [[ $key == "jobsAPIPort" ]] && [[ $section == "zos-services" ]] 
             then
@@ -180,6 +186,11 @@ then
     ZOWE_ROOT_DIR="~/zowe/$ZOWE_VERSION"
     echo "  ZOWE_ROOT_DIR not specified:  Defaulting to ~/zowe/$ZOWE_VERSION"
 fi
+if [[ $ZOWE_PREFIX == "" ]]
+then
+    ZOWE_PREFIX="ZOWE"
+    echo "  ZOWE_PREFIX not specified:  Defaulting to ZOWE"
+fi
 if [[ $ZOWE_EXPLORER_SERVER_JOBS_PORT == "" ]]
 then
     ZOWE_EXPLORER_SERVER_JOBS_PORT=7080
@@ -274,6 +285,7 @@ then
 fi
 
 echo "  ZOWE_ROOT_DIR="$ZOWE_ROOT_DIR >> $LOG_FILE
+echo "  ZOWE_PREFIX="$ZOWE_PREFIX >> $LOG_FILE
 echo "  ZOWE_ZLUX_SERVER_HTTPS_PORT="$ZOWE_ZLUX_SERVER_HTTPS_PORT >> $LOG_FILE
 echo "  ZOWE_EXPLORER_SERVER_JOBS_PORT="$ZOWE_EXPLORER_SERVER_JOBS_PORT >> $LOG_FILE
 echo "  ZOWE_EXPLORER_SERVER_DATASETS_PORT="$ZOWE_EXPLORER_SERVER_DATASETS_PORT >> $LOG_FILE
