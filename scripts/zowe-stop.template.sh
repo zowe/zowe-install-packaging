30b93d5a9c02d3e346e4e9628aa02b15692b164f
John Davies <daviesja@uk.ibm.com>
2019-08-23 14:35:27 +0100

    Implement new STC naming scheme as per #582
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/zowe-stop.template.sh
+++ scripts/zowe-stop.template.sh
@@ -15,4 +15,4 @@ VAR=`dirname $0`			# Obtain the scripts directory name
 cd $VAR/..				# Change to its parent which should be ZOWE_ROOT_DIR
 ZOWE_ROOT_DIR=`pwd`			# Set our environment variable
 echo Stopping ZOWE server
-$ZOWE_ROOT_DIR/scripts/internal/opercmd "c {{zowe_prefix}}SV1"
+$ZOWE_ROOT_DIR/scripts/internal/opercmd "c {{zowe_prefix}}{{zowe_instance}}SV"
