eaee0cd089d448f8e81d1423cd79eb49284810c0
stevenhorsman <steven@uk.ibm.com>
2019-09-12 14:44:20 +0100

    Fix staticApiDefinitionsDirectories property
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/run-zowe.template.sh
+++ scripts/run-zowe.template.sh
@@ -70,7 +70,7 @@ mkdir -p ${USER_DIR}/api-defs
 STATIC_DEF_CONFIG_DIR=${USER_DIR}/api-defs
 
 # TODO - temporary until APIML is componentised - Inject it into discovery script
-sed -e "s#-Dapiml.discovery.staticApiDefinitionsDirectories.*[^\\]#-Dapiml.discovery.staticApiDefinitionsDirectories=${STATIC_DEF_CONFIG_DIR};${ZOWE_ROOT_DIR}/api-mediation/api-defs #" \
+sed -e "s#-Dapiml.discovery.staticApiDefinitionsDirectories.*[^\\]#-Dapiml.discovery.staticApiDefinitionsDirectories=\"${STATIC_DEF_CONFIG_DIR};${ROOT_DIR}/api-mediation/api-defs\" #" \
   "${ROOT_DIR}/api-mediation/scripts/api-mediation-start-discovery.sh" \
   > "${ROOT_DIR}/api-mediation/scripts/api-mediation-start-discovery.sh.copy"
 mv "${ROOT_DIR}/api-mediation/scripts/api-mediation-start-discovery.sh.copy" "${ROOT_DIR}/api-mediation/scripts/api-mediation-start-discovery.sh"
