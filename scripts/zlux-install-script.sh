18faca2301f4885a4a3f25495e633e591eb1f0c2
Suneeth Keerthy <skeerthy@rocketsoftware.com>
2019-09-18 16:46:47 -0400

    Created default allowed plugins json
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- scripts/zlux-install-script.sh
+++ scripts/zlux-install-script.sh
@@ -31,6 +31,7 @@ done
 
 mkdir -p zlux-app-server/pluginDefaults/org.zowe.zlux.ng2desktop/ui/launchbar/plugins
 cp -f ${INSTALL_DIR}/files/zlux/config/pinnedPlugins.json zlux-app-server/pluginDefaults/org.zowe.zlux.ng2desktop/ui/launchbar/plugins/
+cp -f ${INSTALL_DIR}/files/zlux/config/allowedPlugins.json zlux-app-server/pluginDefaults/org.zowe.zlux.bootstrap/plugins/
 cp -f ${INSTALL_DIR}/files/zlux/config/zluxserver.json zlux-app-server/config/
 cp -f ${INSTALL_DIR}/files/zlux/config/plugins/* zlux-app-server/plugins/
 
