22d0606630b7f570cef7058c3e9cd34311c367b1
stevenhorsman <steven@uk.ibm.com>
2019-09-27 16:45:30 +0100

    Add keystore setup script template back in
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/configure/zowe-configure-api-mediation.sh
+++ scripts/configure/zowe-configure-api-mediation.sh
@@ -36,6 +36,29 @@ cd $API_MEDIATION_DIR
 STATIC_DEF_CONFIG=$API_MEDIATION_DIR"/api-defs"
 mkdir -p $STATIC_DEF_CONFIG
 
+echo "About to set JAVA_HOME to $ZOWE_JAVA_HOME in APIML script templates" >> $LOG_FILE
+
+cd scripts/
+# Add JAVA_HOME to both script templates
+sed -e "s|\*\*JAVA_SETUP\*\*|export JAVA_HOME=$ZOWE_JAVA_HOME|g" \
+    -e "s/\*\*HOSTNAME\*\*/$ZOWE_EXPLORER_HOST/g" \
+    -e "s/\*\*IPADDRESS\*\*/$ZOWE_IPADDRESS/g" \
+    -e "s/\*\*VERIFY_CERTIFICATES\*\*/$ZOWE_APIM_VERIFY_CERTIFICATES/g" \
+    -e "s/\*\*ZOSMF_KEYRING\*\*/$ZOWE_ZOSMF_KEYRING/g" \
+    -e "s/\*\*ZOSMF_USER\*\*/$ZOWE_ZOSMF_USERID/g" \
+    -e "s|\*\*EXTERNAL_CERTIFICATE\*\*|$ZOWE_APIM_EXTERNAL_CERTIFICATE|g" \
+    -e "s|\*\*EXTERNAL_CERTIFICATE_ALIAS\*\*|$ZOWE_APIM_EXTERNAL_CERTIFICATE_ALIAS|g" \
+    -e "s|\*\*EXTERNAL_CERTIFICATE_AUTHORITIES\*\*|$ZOWE_APIM_EXTERNAL_CERTIFICATE_AUTHORITIES|g" \
+    -e "s|\*\*ZOWE_ROOT_DIR\*\*|$ZOWE_ROOT_DIR|g" \
+    setup-apiml-certificates-template.sh > setup-apiml-certificates.sh
+
+# Make configured script executable
+chmod a+x setup-apiml-certificates.sh
+
+# Make the scripts read and executable
+chmod -R 750 "${API_MEDIATION_DIR}/scripts"
+
+cd ..
 
 # Execute the APIML certificate generation - no user input required
 echo "  Setting up Zowe API Mediation Layer certificates..."
