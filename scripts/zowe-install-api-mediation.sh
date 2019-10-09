5fae334510844a6cd323a203a6f9f642fa435950
OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>
2019-10-09 02:20:00 -0400

    update zowe-install-api-mediation.sh
    Signed-off-by: OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>

--- scripts/zowe-install-api-mediation.sh
+++ scripts/zowe-install-api-mediation.sh
@@ -1,53 +1,107 @@
 #!/bin/sh
-
-################################################################################
-# This program and the accompanying materials are made available under the terms of the
-# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+#######################################################################
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v2.0 which
+# accompanies this distribution, and is available at
 # https://www.eclipse.org/legal/epl-v20.html
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2018, 2019
-################################################################################
+# Copyright Contributors to the Zowe Project. 2018, 2019
+#######################################################################
 
-#********************************************************************
+# Install the Zowe API Mediation Layer.
+# Called by zowe-install.sh
+#
+# Arguments:
+# /
+#
 # Expected globals:
-# $ZOWE_JAVA_HOME
-# $ZOWE_ROOT_DIR
+# $ReMoVe $IgNoRe_ErRoR $debug $LOG_FILE $INSTALL_DIR
 
-API_MEDIATION_DIR=$ZOWE_ROOT_DIR"/components/api-mediation"
+here=$(cd $(dirname $0);pwd)   # script location
+me=$(basename $0)              # script name
+#debug=-d                      # -d or null, -d triggers early debug
+#IgNoRe_ErRoR=1                # no exit on error when not null  #debug
+#set -x                                                          #debug
 
-echo "<zowe-api-mediation-install.sh>" >> $LOG_FILE
-cd $INSTALL_DIR
-API_MEDIATION_PAX=$PWD/$(ls -t ./files/api-mediation-package*.pax | head -1)
-if [ ! -f $API_MEDIATION_PAX ]
-	then
-	    echo "Api Mediation PAX Archive (api-mediation-package*.pax) missing"
-	    echo "Installation terminated"
-	    exit 0
-fi
+echo "-- API Mediation"
+test "$debug" && echo "> $me $@"
+test "$LOG_FILE" && echo "<$me> $@" >> $LOG_FILE
 
-# unpax the API Mediation services
-echo "Installing API Mediation into ${API_MEDIATION_DIR} ...">> $LOG_FILE
-umask 0002
-mkdir -p ${API_MEDIATION_DIR}
-cd ${API_MEDIATION_DIR}
-# Change to the place where we are expanding the .pax into the /api-mediation beneath the $rootDir environment variable, e.g /usr/lpp/zowe
-echo "Unpax of $API_MEDIATION_PAX into $PWD" >> $LOG_FILE
-pax -rf $API_MEDIATION_PAX -ppx
+# ---------------------------------------------------------------------
+# --- show & execute command, and bail with message on error
+#     stderr is routed to stdout to preserve the order of messages
+# $1: if --null then trash stdout, parm is removed when present
+# $1: if --save then append stdout to $2, parms are removed when present
+# $1: if --repl then save stdout to $2, parms are removed when present
+# $2: if $1 = --save or --repl then target receiving stdout
+# $@: command with arguments to execute
+# ---------------------------------------------------------------------
+function _cmd
+{
+test "$debug" && echo
+if test "$1" = "--null"
+then         # stdout -> null, stderr -> stdout (without going to null)
+  shift
+  test "$debug" && echo "$@ 2>&1 >/dev/null"
+                         $@ 2>&1 >/dev/null
+elif test "$1" = "--save"
+then         # stdout -> >>$2, stderr -> stdout (without going to $2)
+  sAvE=$2
+  shift 2
+  test "$debug" && echo "$@ 2>&1 >> $sAvE"
+                         $@ 2>&1 >> $sAvE
+elif test "$1" = "--repl"
+then         # stdout -> >$2, stderr -> stdout (without going to $2)
+  sAvE=$2
+  shift 2
+  test "$debug" && echo "$@ 2>&1 > $sAvE"
+                         $@ 2>&1 > $sAvE
+else         # stderr -> stdout, caller can add >/dev/null to trash all
+  test "$debug" && echo "$@ 2>&1"
+                         $@ 2>&1
+fi    #
+sTaTuS=$?
+if test $sTaTuS -ne 0
+then
+  echo "** ERROR $me '$@' ended with status $sTaTuS"
+  test ! "$IgNoRe_ErRoR" && exit 8                               # EXIT
+fi    #
+}    # _cmd
 
-# TODO are these mediation steps needed (from api-ml config script)?
+# ---------------------------------------------------------------------
+# --- main --- main --- main --- main --- main --- main --- main ---
+# ---------------------------------------------------------------------
+function main { }     # dummy function to simplify program flow parsing
+_cmd umask 0022                                  # similar to chmod 755
 
-# Set a+rx for API Mediation JARs
-chmod a+rx "${API_MEDIATION_DIR}"/*.jar 
-chmod -R 751 "${API_MEDIATION_DIR}/bin"
+# Set environment variables when not called via zowe-install.sh
+if test -z "$INSTALL_DIR"
+then
+  # Set all required environment variables & logging
+  # Note: script exports environment vars, so run in current shell
+  _cmd . $(dirname $0)/../scripts/zowe-set-envvars.sh $0
+else
+  echo "  $(date)" >> $LOG_FILE
+fi    #
 
-# Make the apiml-auth plugin readable by everyone
-chmod a+rx "${API_MEDIATION_DIR}/apiml-auth"
-chmod a+rx "${API_MEDIATION_DIR}/apiml-auth/lib"
-chmod -R a+r "${API_MEDIATION_DIR}/apiml-auth"
+# Target paths based on $ZOWE_ROOT_DIR
+folder="api-mediation"
 
-# TODO - move image into apiml pax
-cp $INSTALL_DIR/files/assets/api-catalog.png ${API_MEDIATION_DIR}
+_cmd $scripts/unpax.sh \
+  "$INSTALL_DIR/files/api-mediation-package*.pax" \
+  "$ZOWE_ROOT_DIR/$folder" \
+  "API Mediation"
 
-echo "</zowe-api-mediation-install.sh>" >> $LOG_FILE
+# TODO move image into apiml pax
+_cmd cp -f $INSTALL_DIR/files/assets/api-catalog.png $ZOWE_ROOT_DIR/$folder
+# Remove install source if requested
+test "$ReMoVe" && _cmd rm -f $INSTALL_DIR/files/assets/api-catalog.png
+
+# Remove install script if requested
+test "$ReMoVe" && _cmd rm -f $0
+
+test "$debug" && echo "< $me 0"
+echo "</$me> 0" >> $LOG_FILE
+exit 0
