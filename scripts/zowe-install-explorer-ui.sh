669a097f8dff1b5ce7c31569164fcaca3a477822
OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>
2019-10-09 03:05:56 -0400

    update zowe-install-explorer-ui.sh
    Signed-off-by: OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>

--- scripts/zowe-install-explorer-ui.sh
+++ scripts/zowe-install-explorer-ui.sh
@@ -1,44 +1,153 @@
 #!/bin/sh
-
-################################################################################
-# This program and the accompanying materials are made available under the terms of the
-# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+#######################################################################
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v2.0 which
+# accompanies this distribution, and is available at
 # https://www.eclipse.org/legal/epl-v20.html
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2018, 2019
-################################################################################
+# Copyright Contributors to the Zowe Project. 2018, 2019
+#######################################################################
 
-#********************************************************************
+# Install the Zowe Explorer UI.
+# Called by zowe-install.sh
+#
+# Arguments:
+# /
+#
 # Expected globals:
-# $ZOWE_ROOT_DIR
-# $INSTALL_DIR
-# $LOG_FILE
+# $ReMoVe $IgNoRe_ErRoR $debug $LOG_FILE $INSTALL_DIR
 
-echo "<zowe-explorer-ui-install.sh>" >> $LOG_FILE
+here=$(cd $(dirname $0);pwd)   # script location
+me=$(basename $0)              # script name
+#debug=-d                      # -d or null, -d triggers early debug
+#IgNoRe_ErRoR=1                # no exit on error when not null  #debug
+#set -x                                                          #debug
 
-UI_PLUGIN_LIST="jes mvs uss"
-for one in $UI_PLUGIN_LIST; do
-  EXPLORER_PLUGIN_UPPERCASE=$(echo $one | tr '[a-z]' '[A-Z]')
+echo "-- Explorer UI"
+test "$debug" && echo "> $me $@"
+test "$LOG_FILE" && echo "<$me> $@" >> $LOG_FILE
 
-  cd $INSTALL_DIR
-  EXPLORER_PLUGIN_PAX=$PWD/$(ls -t ./files/explorer-${one}-*.pax | head -1)
-  if [ ! -f $EXPLORER_PLUGIN_PAX ]; then
-    echo "  ${EXPLORER_PLUGIN_UPPERCASE} Explorer UI (explorer-${one}-*.pax) missing"
-    echo "  Installation terminated"
-    exit 0
-  fi
+# ---------------------------------------------------------------------
+# --- install UI server code & scripts
+# $1: UI name
+# ---------------------------------------------------------------------
+function _install
+{
+test "$debug" && echo "> _install $@"
+name=$1
+NAME=$(echo $name | tr '[:lower:]' '[:upper:]')             # uppercase
 
-  # NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-  EXPLORER_INSTALL_FOLDER="${one}_explorer"
-  echo "  Installing Explorer UI ${EXPLORER_PLUGIN_UPPERCASE} into ${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
-  umask 0002
-  mkdir -p "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-  # unpax package
-  cd "${ZOWE_ROOT_DIR}/${EXPLORER_INSTALL_FOLDER}"
-  echo "  Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
-  pax -rf $EXPLORER_PLUGIN_PAX -ppx
-done
+test "$debug" && echo "processing $NAME UI"
+echo "  processing $NAME UI" >> $LOG_FILE
 
-echo "</zowe-explorer-ui-install.sh>" >> $LOG_FILE
+# Input paths
+paxMask="$INSTALL_DIR/files/explorer-${name}-*.pax"
+
+# Target paths based on $ZOWE_ROOT_DIR
+folder="components/${name}-explorer/bin"
+
+# Secondary input paths
+# TODO why are these not in $INSTALL_DIR/files/scripts like Explorer-API?
+#start="$ZOWE_ROOT_DIR/$folder/scripts/${name}-explorer-start.sh"
+start="$ZOWE_ROOT_DIR/$folder/scripts/start-explorer-${name}-ui-server.sh"
+configure="$ZOWE_ROOT_DIR/$folder/scripts/${name}-explorer-configure.sh"
+validate="$ZOWE_ROOT_DIR/$folder/scripts/${name}-explorer-validate.sh"
+
+# unpax archive (required)
+_cmd $scripts/unpax.sh \
+  "$paxMask" \
+  "$ZOWE_ROOT_DIR/$folder" \
+  "$NAME Explorer UI"
+
+# Install startup script (required)
+# TODO use mv while script is part of pax instead of $INSTALL_DIR/files/scripts
+#_cp "$start" "$ZOWE_ROOT_DIR/$folder/start.sh"
+_cmd mv "$start" "$ZOWE_ROOT_DIR/$folder/start.sh"
+
+# Install configure script (optional)
+if test -f "$configure" 
+then
+# TODO use mv while script is part of pax instead of $INSTALL_DIR/files/scripts
+#  _cp "$configure" "$ZOWE_ROOT_DIR/$folder/configure.sh"
+  _cmd mv "$configure" "$ZOWE_ROOT_DIR/$folder/configure.sh"
+fi    #
+
+# Install validate script (optional)
+if test -f "$validate" 
+then
+# TODO use mv while script is part of pax instead of $INSTALL_DIR/files/scripts
+#  _cp "$validate" "$ZOWE_ROOT_DIR/$folder/validate.sh"
+  _cmd mv "$validate" "$ZOWE_ROOT_DIR/$folder/validate.sh"
+fi    #
+
+test "$debug" && echo "< _install"
+}    # _install
+
+# ---------------------------------------------------------------------
+# --- show & execute command, and bail with message on error
+#     stderr is routed to stdout to preserve the order of messages
+# $1: if --null then trash stdout, parm is removed when present
+# $1: if --save then append stdout to $2, parms are removed when present
+# $1: if --repl then save stdout to $2, parms are removed when present
+# $2: if $1 = --save or --repl then target receiving stdout
+# $@: command with arguments to execute
+# ---------------------------------------------------------------------
+function _cmd
+{
+test "$debug" && echo
+if test "$1" = "--null"
+then         # stdout -> null, stderr -> stdout (without going to null)
+  shift
+  test "$debug" && echo "$@ 2>&1 >/dev/null"
+                         $@ 2>&1 >/dev/null
+elif test "$1" = "--save"
+then         # stdout -> >>$2, stderr -> stdout (without going to $2)
+  sAvE=$2
+  shift 2
+  test "$debug" && echo "$@ 2>&1 >> $sAvE"
+                         $@ 2>&1 >> $sAvE
+elif test "$1" = "--repl"
+then         # stdout -> >$2, stderr -> stdout (without going to $2)
+  sAvE=$2
+  shift 2
+  test "$debug" && echo "$@ 2>&1 > $sAvE"
+                         $@ 2>&1 > $sAvE
+else         # stderr -> stdout, caller can add >/dev/null to trash all
+  test "$debug" && echo "$@ 2>&1"
+                         $@ 2>&1
+fi    #
+sTaTuS=$?
+if test $sTaTuS -ne 0
+then
+  echo "** ERROR $me '$@' ended with status $sTaTuS"
+  test ! "$IgNoRe_ErRoR" && exit 8                               # EXIT
+fi    #
+}    # _cmd
+
+# ---------------------------------------------------------------------
+# --- main --- main --- main --- main --- main --- main --- main ---
+# ---------------------------------------------------------------------
+function main { }     # dummy function to simplify program flow parsing
+_cmd umask 0022                                  # similar to chmod 755
+
+# Set environment variables when not called via zowe-install.sh
+if test -z "$INSTALL_DIR"
+then
+  # Set all required environment variables & logging
+  # Note: script exports environment vars, so run in current shell
+  _cmd . $(dirname $0)/../scripts/zowe-set-envvars.sh $0
+else
+  echo "  $(date)" >> $LOG_FILE
+fi    #
+
+#  Install the different Explorer UIs
+_install jes
+
+# Remove install script if requested
+test "$ReMoVe" && _cmd rm -f $0
+
+test "$debug" && echo "< $me 0"
+echo "</$me> 0" >> $LOG_FILE
+exit 0
