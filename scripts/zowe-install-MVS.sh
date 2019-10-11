6fed33a92df0aa1a22fcc99fa81ba13bf9990f92
OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>
2019-10-11 18:32:36 -0400

    update zowe-install-MVS.sh
    Signed-off-by: OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>

--- scripts/zowe-install-MVS.sh
+++ scripts/zowe-install-MVS.sh
@@ -1,38 +1,84 @@
 #!/bin/sh
-
-################################################################################
-# This program and the accompanying materials are made available under the terms of the
-# Eclipse Public License v2.0 which accompanies this distribution, and is available at
+#######################################################################
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v2.0 which
+# accompanies this distribution, and is available at
 # https://www.eclipse.org/legal/epl-v20.html
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2019, 2019
-################################################################################
+# Copyright Contributors to the Zowe Project. 2019, 2019
+#######################################################################
 
-# create MVS artefacts 
-script=zowe-create-MVS-artefacts.sh
-echo "<$script>" >> $LOG_FILE 
-echo "Creating MVS artefacts ... " >> $LOG_FILE 
+size.SZWEAUTH='space(15,15) tracks'
+size.SZWESAMP='space(15,15) tracks'
+files.templates='ZOWESVR.template.jcl ZWESECUR.jcl'
 
-# Unpax the LOADLIB and SAMPLIB
+# info: construct ${variable%%.*} keeps up to first . (exclusive)
+
+# create MVS artifacts
+script=zowe-install-MVS.sh
+echo "<$script>" >> $LOG_FILE
+echo "Creating MVS artifacts ... " >> $LOG_FILE
+
+# Unpax the ZSS LOADLIB and SAMPLIB
 echo "Unpax the LOADLIB and SAMPLIB:" >> ${LOG_FILE}
-mkdir -p ${TEMP_DIR}/files/zss/LOADLIB >> ${LOG_FILE} 2>&1
-mkdir -p ${TEMP_DIR}/files/zss/SAMPLIB >> ${LOG_FILE} 2>&1
+mkdir -p ${TEMP_DIR}/${script%%.*}/LOADLIB >> ${LOG_FILE} 2>&1
+mkdir -p ${TEMP_DIR}/${script%%.*}/SAMPLIB >> ${LOG_FILE} 2>&1
 dir=`pwd`
-cd ${TEMP_DIR}/files/zss
+cd ${TEMP_DIR}/${script%%.*}
 pax -rvf ${INSTALL_DIR}/files/zss.pax -ppx LOADLIB >> ${LOG_FILE} 2>&1
 pax -rvf ${INSTALL_DIR}/files/zss.pax -ppx SAMPLIB >> ${LOG_FILE} 2>&1
-cd $dir 
+cd $dir
+
+# add non-ZSS members to staging area
+
+for file in $files.templates
+do
+  cp $INSTALL_DIR/files/templates/$file ${TEMP_DIR}/${script%%.*}/SAMPLIB/${file%%.*}
+  rc=$?
+  if test $rc -ne 0
+  then
+    echo "  $script $file not staged in ${TEMP_DIR}/${script%%.*}/SAMPLIB, RC=$rc" >> $LOG_FILE
+  fi
+done
+
+# TODO remove once zowe/zss/samplib/zis/ is updated
+# >>>>
+# adjust ZSS samples
+rm -f ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESISMS
+mv ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESIS01 ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESISTC
+if test!  -f ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESIPRG
+then
+# Statements below must not exceed col 80
+#----------------------------------------------------------------------------80|
+cat > ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESIPRG <<EndOfZWESIPRG
+/* issue this console command to authorize the loadlib temporarily */
+SETPROG APF,ADD,DSNAME=${ZOWE_DSN_PREFIX}.SZWEAUTH,VOLUME=${volume}
+/* Add this statement to SYS1.PARMLIB(PROGxx) or equivalent
+   to authorize the loadlib permanently */
+APF ADD DSNAME(${ZOWE_DSN_PREFIX}.SZWEAUTH) VOLUME(${volume})
+EndOfZWESIPRG
+#----------------------------------------------------------------------------80|
+fi
+# <<<<
+
+# TODO remove once zowe-install-packaging/files/templates is updated
+# >>>>
+# adjust non-ZSS samples
+mv ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZOWESVR ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESTC
+# <<<<
+
 
 # 1. {datasetprefix}.SZWEAUTH
-#      ZWESIS01 
 
 tsocmd "delete '${ZOWE_DSN_PREFIX}.SZWEAUTH' " >> ${LOG_FILE} 2>&1
 
+# SZWEAUTH must be PDSE
+# TODO replace by allocate-dataset.sh call to resuse VOLSER support
 tsocmd "allocate new da('${ZOWE_DSN_PREFIX}.SZWEAUTH') " \
     "dsntype(library) dsorg(po) recfm(u) lrecl(0) blksize(6999)" \
-    "space(5,2) tracks unit(sysallda)" >> $LOG_FILE 2>&1
+    "unit(sysallda)" $size.SZWEAUTH >> $LOG_FILE 2>&1
 rc=$?
 if test $rc -eq 0
 then
@@ -40,29 +86,31 @@ then
 
     # copy LOADLIB to PDS
     echo "Copy LOADLIB to PDS:" >> ${LOG_FILE}
-    cp -X -v ${TEMP_DIR}/files/zss/LOADLIB/ZWESIS01 "//'${ZOWE_DSN_PREFIX}.SZWEAUTH(ZWESIS01)'" >> ${LOG_FILE}
-    
-    rc=$?
-    if test $rc -eq 0
-    then
-        echo "  $script ZWESIS01 copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH" >> $LOG_FILE
-    else
-        echo "  $script ZWESIS01 not copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH, RC=$rc" >> $LOG_FILE
-    fi 
+    for member in $(ls ${TEMP_DIR}/${script%%.*}/LOADLIB/)
+    do
+      cp -X -v ${TEMP_DIR}/${script%%.*}/LOADLIB/$member "//'${ZOWE_DSN_PREFIX}.SZWEAUTH'" >> ${LOG_FILE}
 
+      rc=$?
+      if test $rc -eq 0
+      then
+          echo "  $script $member copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH" >> $LOG_FILE
+      else
+          echo "  $script $member not copied to ${ZOWE_DSN_PREFIX}.SZWEAUTH, RC=$rc" >> $LOG_FILE
+      fi
+    done
 else
     echo "  $script failed to create ${ZOWE_DSN_PREFIX}.SZWEAUTH, RC=$rc" >> $LOG_FILE
 fi
 
 
-
-
 # 2. {datasetprefix}.SZWESAMP
 
 tsocmd "delete '${ZOWE_DSN_PREFIX}.SZWESAMP' " >> ${LOG_FILE} 2>&1
+
+# TODO replace by allocate-dataset.sh call to resuse VOLSER support
 tsocmd "allocate new da('${ZOWE_DSN_PREFIX}.SZWESAMP') " \
     "dsntype(library) dsorg(po) recfm(f b) lrecl(80) " \
-    "space(5,2) tracks unit(sysallda)" >> $LOG_FILE 2>&1
+    "unit(sysallda)" $size.SZWESAMP >> $LOG_FILE 2>&1
 rc=$?
 if test $rc -eq 0
 then
@@ -71,40 +119,21 @@ then
     # copy SAMPLIB files to PDS
     echo "Copy SAMPLIB files to PDS:" >> ${LOG_FILE}
 
-    # ... obtain members as yet not in zss.pax file ...
-
-    # cp $INSTALL_DIR/files/templates/ZWESECUR.jcl ${TEMP_DIR}/files/zss/SAMPLIB/ZWESECUR # stagings/cupids
-
-# Statements below must not exceed col 80
-#----------------------------------------------------------------------------80|
-cat > ${TEMP_DIR}/files/zss/SAMPLIB/ZWESIPRG <<EndOfZWESIPRG
-/* issue this console command to authorize the loadlib temporarily */
-SETPROG APF,ADD,DSNAME=${ZOWE_DSN_PREFIX}.SZWEAUTH,VOLUME=${volume}
-/* Add this statement to SYS1.PARMLIB(PROGxx) or equivalent 
-   to authorize the loadlib permanently */
-APF ADD DSNAME(${ZOWE_DSN_PREFIX}.SZWEAUTH) VOLUME(${volume}) 
-EndOfZWESIPRG
-#----------------------------------------------------------------------------80|
-
-    mv ${TEMP_DIR}/files/zss/SAMPLIB/ZWESIS01 ${TEMP_DIR}/files/zss/SAMPLIB/ZWESISTC
-
-    cp $INSTALL_DIR/files/templates/ZOWESVR.template.jcl ${TEMP_DIR}/files/zss/SAMPLIB/ZOWESVR # unconfigured
-    
-    for member in ZWESIP00 ZWESISCH ZWESISMS ZOWESVR  ZWESISTC ZWESIPRG # ZWESECUR (will come from stagings/cupids)
+    for member in $(ls ${TEMP_DIR}/${script%%.*}/SAMPLIB/)
     do
-        cp -v ${TEMP_DIR}/files/zss/SAMPLIB/$member "//'${ZOWE_DSN_PREFIX}.SZWESAMP($member)'" >> ${LOG_FILE}
+        cp -v ${TEMP_DIR}/${script%%.*}/SAMPLIB/$member "//'${ZOWE_DSN_PREFIX}.SZWESAMP'" >> ${LOG_FILE}
         rc=$?
         if test $rc -eq 0
         then
             echo "  $script $member copied to ${ZOWE_DSN_PREFIX}.SZWESAMP" >> $LOG_FILE
         else
             echo "  $script $member not copied to ${ZOWE_DSN_PREFIX}.SZWESAMP, RC=$rc" >> $LOG_FILE
-        fi 
+        fi
     done
 else
     echo "  $script failed to create ${ZOWE_DSN_PREFIX}.SZWESAMP, RC=$rc" >> $LOG_FILE
 fi
 
-rm -rf $TEMP_DIR
+rm -rf $TEMP_DIR/${script%%.*}
 
-echo "</$script>" >> $LOG_FILE 
+echo "</$script>" >> $LOG_FILE
