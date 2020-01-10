b5e8f3012f4d073993bb9505ed6c52f15a003e65
John Davies <daviesja@uk.ibm.com>
2020-01-10 14:57:50 +0000

    replace comment
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/zowe-install-MVS.sh
+++ scripts/zowe-install-MVS.sh
@@ -49,6 +49,7 @@ done
 rm -f ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESISMS
 mv ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESIS01 ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESISTC
 mv ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESAUX ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESASTC
+# <<<<
 
 if test ! -f ${TEMP_DIR}/${script%%.*}/SAMPLIB/ZWESIPRG
 then
