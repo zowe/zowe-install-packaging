33e6d076b76adaeb3a17bddf3e8cab630c531ebc
stevenhorsman <steven@uk.ibm.com>
2020-01-20 15:37:07 +0000

    Update redirect logic
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/utils/error.sh
+++ scripts/utils/error.sh
@@ -17,5 +17,7 @@ then
   ERRORS_FOUND=0
 fi
 
-echo "Error $ERRORS_FOUND: $1" | tee >(cat >&2)
+# echo error to standard out and err
+echo "Error $ERRORS_FOUND: $1"
+echo "Error $ERRORS_FOUND: $1" 1>&2
 let "ERRORS_FOUND=$ERRORS_FOUND+1"
