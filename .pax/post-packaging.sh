b9a505911826ad39234f1e6280849eb691692636
OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>
2020-01-26 22:02:23 -0500

    add cd command
    Signed-off-by: OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>

--- .pax/post-packaging.sh
+++ .pax/post-packaging.sh
@@ -78,7 +78,9 @@ done
 USS_PATH="${PAX_PATH}/USS"        # output
 LOCAL_PATH="${ZOSMF_PATH}/vtls"   # input
 cp ${LOCAL_PATH}/ZWEYML01.yml ${USS_PATH}/ZWEYML01.yml
-${ZOSMF_PATH}/build-workflow.rex -d -i ${ZOSMF_PATH}/smpe_workflow.xml -o ${USS_PATH}/ZWEWRF01.xml
+cd ${ZOSMF_PATH}   # required, smpe_workflow.xml has ./vtls references 
+./build-workflow.rex -d -i ./smpe_workflow.xml -o ${USS_PATH}/ZWEWRF01.xml
+cd ${CURR_PWD}
 
 # create smpe.pax
 cd ${CURR_PWD}/smpe/pax
