663d5c03a61c2996fba7d3a8e9027afe011859e8
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-04-20 17:23:13 -0400

    test security systems - acf2/ts
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- tests/installation/jest.config.js
+++ tests/installation/jest.config.js
@@ -1,4 +1,15 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
 module.exports = {
+  runner: "./dist/worker-group-runner.js",
   reporters: [
     "default",
     [
---
+++ tests/installation/src/__tests__/extented/security-systems/convenience/racf.ts
@@ -0,0 +1,41 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import {
+  checkMandatoryEnvironmentVariables,
+  installAndVerifyConvenienceBuild,
+} from '../../../../utils';
+import { TEST_TIMEOUT_CONVENIENCE_BUILD } from '../../../../constants';
+
+/**
+ * Define this test should run in a specific worker
+ *
+ * @worker marist-1
+ */
+const testSuiteName = 'Test convenience build installation with RACF';
+describe(testSuiteName, () => {
+  beforeAll(() => {
+    // validate variables
+    checkMandatoryEnvironmentVariables([
+      'ZOWE_BUILD_LOCAL',
+    ]);
+  });
+
+  test('install and verify', async () => {
+    await installAndVerifyConvenienceBuild(
+      testSuiteName,
+      // hard code to use marist-1 which we started with ACF2
+      'marist-1',
+      {
+        'zowe_build_local': process.env['ZOWE_BUILD_LOCAL'],
+      }
+    );
+  }, TEST_TIMEOUT_CONVENIENCE_BUILD);
+});
