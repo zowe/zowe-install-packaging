8f91a6a14233173793fbf2cee85ec4ab8b0a71d3
stevenhorsman <steven@uk.ibm.com>
2020-04-22 10:23:03 +0100

    Fix identation
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- bin/utils/setup-log-dir.sh
+++ bin/utils/setup-log-dir.sh
@@ -27,7 +27,7 @@ then
   then 
     LOG_DIRECTORY="/global/zowe/logs"
   else
-	LOG_DIRECTORY=`sh -c "echo ~/zowe/logs"`
+	  LOG_DIRECTORY=`sh -c "echo ~/zowe/logs"`
   fi
 else
   # If the path is relative, then expand it
