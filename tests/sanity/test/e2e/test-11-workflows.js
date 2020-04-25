192b30263411115390a13cdcdae8d78c7fd0c86e
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-04-24 21:12:25 -0400

    workflow app is not started by default now
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- tests/sanity/test/e2e/test-11-workflows.js
+++ tests/sanity/test/e2e/test-11-workflows.js
@@ -27,7 +27,7 @@ let driver;
 const APP_TO_TEST = 'User Tasks/Workflows';
 
 
-describe(`test ${APP_TO_TEST}`, function() {
+describe.skip(`test ${APP_TO_TEST}`, function() {
   before('verify environment variable and load login page', async function() {
     expect(process.env.SSH_HOST, 'SSH_HOST is not defined').to.not.be.empty;
     expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
