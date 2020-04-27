007455870cf0a344f352acd3571f75a4b021c35c
stevenhorsman <steven@uk.ibm.com>
2020-04-27 11:51:04 +0100

    Fix message
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- install/zowe-install.sh
+++ install/zowe-install.sh
@@ -11,7 +11,7 @@
 ################################################################################
 
 if [ $# -lt 4 ]; then
-  echo "Usage: $0 -i <zowe_install_path> -h <zowe_dsn_prefix> [-l <log_directory>"
+  echo "Usage: $0 -i <zowe_install_path> -h <zowe_dsn_prefix> [-l <log_directory>]"
   exit 1
 fi
 
