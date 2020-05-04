152c9a1a6556f8047d734de34f04765d6e966a4f
John Davies <daviesja@uk.ibm.com>
2020-05-04 10:41:34 +0100

    check I/O
    Signed-off-by: John Davies <daviesja@uk.ibm.com>
    Signed-off-by: Jiri Cizek <jiri.cizek@broadcom.com>

---
+++ scripts/utils/zowe-checksum-runtime.sh
@@ -0,0 +1,82 @@
+
+#!/bin/sh
+#######################################################################
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v2.0 which
+# accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright Contributors to the Zowe Project. 2020
+#######################################################################
+
+#	zowe-checksum-runtime.sh
+# 
+
+# Function: Create the checksum files of the runtime directory files.
+# Method  : Create a hash of every file in the driver 
+# Inputs  - pathname of Zowe runtime driver on the build's z/OS system
+#           This might be $ROOT_DIR in Jenkins?
+#         - pathname where the source of HashFiles.java is kept
+#           This is currently zowe-install-packaging/scripts/utils
+# Note about SMP/E
+#         - The directory SMPE in the runtime folder is excluded from this check
+#           The SMPE directory and contents must not be present in the supplied
+#           RefRuntimeHash.txt file, or they will be flagged as extra.  
+
+# Outputs 
+#           •	HashFiles.class (binary)
+#           •	RefRuntimeHash.txt (list of files with hash keys)
+#           •	RefRuntimeHashHash.txt (hash of the above list)
+
+#           
+# Uses    - java compiler
+SCRIPT=zowe-checksum-runtime.sh
+
+if [[ $# -ne 2 ]]   
+then
+echo; echo $SCRIPT Usage:
+cat <<EndOfUsage
+$SCRIPT runtimePath hashPath
+
+   Parameter subsitutions:
+ 
+    Parm name       Sample value    Meaning
+    ---------       ------------    -------
+ 1  runtimePath     /usr/lpp/zowe   root directory for the executables used by Zowe at run time
+ 2  hashPath        scripts/utils   writable work directory where you want the reference hash key files and program created
+
+EndOfUsage
+exit
+fi
+
+cd $hashPath
+javac HashFiles.java 
+cd $runtimePath
+
+# create a list of files to be hashed
+find . -name ./SMPE -prune \
+    -o -name "./ZWE*" -prune \
+    -o -type f -print > $hashPath/files.in # exclude SMPE
+# create the main set of hash files
+java -cp $hashPath HashFiles $hashPath/files.in > $hashPath/RefRuntimeHash.txt
+echo RefRuntimeHash RC=$?
+
+# create the single hash of those hash files     
+cd $hashPath
+echo RefRuntimeHash.txt > files.in
+java HashFiles files.in > RefRuntimeHashHash.txt
+echo RefRuntimeHashHash RC=$?
+
+# Publish via Jenkinsfile:
+
+#   pipeline.publish(
+#     artifacts: [
+#       '$hashPath/HashFiles.class',
+#       '$hashPath/RefRuntimeHash.txt',
+#       '$hashPath/RefRuntimeHashHash.txt'
+#     ]
+#   )
+
+rm files.in
