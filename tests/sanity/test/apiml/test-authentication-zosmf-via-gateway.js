149cf5df5b46d90a9107b18a28d697672b3a0862
Jakub Balhar <jakub.balhar@broadcom.com>
2020-05-06 11:22:56 +0200

    Get valid response for valid Basic Authorization Get valid response for valid Cookie based authentication
    Signed-off-by: Jakub Balhar <jakub.balhar@broadcom.com>

--- tests/sanity/test/apiml/test-authentication-zosmf-via-gateway.js
+++ tests/sanity/test/apiml/test-authentication-zosmf-via-gateway.js
@@ -12,37 +12,80 @@ const _ = require('lodash');
 const expect = require('chai').expect;
 const debug = require('debug')('zowe-sanity-test:apiml:gateway');
 const axios = require('axios');
-const addContext = require('mochawesome/addContext');
 
-let REQ, username, password;
-let cookies = {};
+let request, username, password;
 const APIML_AUTH_COOKIE = 'apimlAuthenticationToken';
 
 // allow self signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+let login = async () => {
+  let response = await request.post('/api/v1/apicatalog/auth/login', {
+    username, password
+  });
+
+  // Validate the response at least basically
+  expect(response.status).to.be.oneOf([200, 204]);
+  expect(response.headers).to.be.an('object');
+  expect(response.headers).to.have.property('set-cookie');
+  expect(response.data).to.be.empty;
+
+  // Retrieve the Api ML authentication cookie
+  let cookiesSetByServer = response.headers['set-cookie'];
+  let authenticationCookie = cookiesSetByServer.filter(cookieRow => cookieRow.startsWith(APIML_AUTH_COOKIE));
+  if(authenticationCookie.length === 0) {
+    throw new Error("The authentication was unsuccessful");
+  }
+
+  return authenticationCookie[0];
+};
+
 describe('test api mediation layer zosmf authentication', function() {
   before('verify environment variables', function() {
-    expect(process.env.SSH_HOST, 'SSH_HOST is not defined').to.not.be.empty;
-    expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
-    expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
-    expect(process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT, 'ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT is not defined').to.not.be.empty;
+    const environment = process.env;
+    expect(environment.SSH_HOST, 'SSH_HOST is not defined').to.not.be.empty;
+    expect(environment.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
+    expect(environment.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
+    expect(environment.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT, 'ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT is not defined').to.not.be.empty;
 
-    REQ = axios.create({
-      baseURL: `https://${process.env.SSH_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`,
+    const baseUrl = `https://${environment.SSH_HOST}:${environment.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`;
+    request = axios.create({
+      baseURL: baseUrl,
       timeout: 30000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-    username = process.env.SSH_USER;
-    password = process.env.SSH_PASSWD;
-    debug(`Explorer server URL: https://${process.env.SSH_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`);
+    username = environment.SSH_USER;
+    password = environment.SSH_PASSWD;
+    debug(`Explorer server URL: ${baseUrl}`);
   });
 
-  it('should be able to get data from ZOSM/f with valid basic header', () => {
+  it('should be able to get data from ZOSM/f with valid basic header', async () => {
+    const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
+    const response = await request.get('/api/v1/zosmf/restfiles/ds?dslevel=sys1.p*', {
+      headers: {
+        'Authorization': `Basic ${token}`,
+        'X-CSRF-ZOSMF-HEADER': '*'
+      }
+    });
 
+    expect(response.status).to.equal(200);
+    expect(response.data).to.not.be.empty;
   });
 
-  it('should be able to get data from ZOSM/f with valid cookie', () => {
+  it('should be able to get data from ZOSM/f with valid cookie', async () => {
+    const authenticationCookie = await login();
 
+    const response = await request.get('/api/v1/zosmf/restfiles/ds?dslevel=sys1.p*', {
+      headers: {
+        'Cookie': authenticationCookie,
+        'X-CSRF-ZOSMF-HEADER': '*'
+      }
+    });
+
+    expect(response.status).to.equal(200);
+    expect(response.data).to.not.be.empty;
   });
 
   it('should be able to get data from ZOSM/f with valid LTPA cookie', () => {
