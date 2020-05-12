faa4741a2f802f2090c6a80a9fb55711d5f06419
stevenhorsman <steven@uk.ibm.com>
2020-05-12 12:43:58 +0100

    More progress
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- bin/utils/node-utils.sh
+++ bin/utils/node-utils.sh
@@ -20,6 +20,46 @@ ensure_node_is_on_path() {
   fi
 }
 
+validate_node_home() {
+  if [[ -n "${NODE_HOME}" ]];
+  then
+      ls ${NODE_HOME}/bin | grep node$ > /dev/null
+      if [[ $? -ne 0 ]];
+      then
+          print_error_message "NODE_HOME: ${NODE_HOME}/bin does not point to a valid install of Node";
+      else
+
+        NODE_OK=`${NODE_HOME}/bin/node -e "console.log('ok')" 2>&1`
+        if [[ ${NODE_OK} == "ok" ]]
+        then
+          echo "OK: Node is working"
+        else 
+          print_error_message "${NODE_HOME}/bin/node is not functioning correctly: ${NODE_OK}";
+        fi
+
+        NODE_MIN_VERSION=6.14
+        NODE_VERSION=`${NODE_HOME}/bin/node --version` 
+        NODE_VERSION_TRIMMED=`${NODE_HOME}/bin/node --version | sed 's/^.\{1\}//' | cut -d. -f1,2 2>&1`
+        if [[ $NODE_VERSION = "v8.16.1" ]];
+        then
+          print_error_message "NODE Version 8.16.1 is not compatible with Zowe. Please use a different version. See https://docs.zowe.org/stable/troubleshoot/app-framework/app-known-issues.html#desktop-apps-fail-to-load for more details";
+        elif [[ `echo "$NODE_VERSION_TRIMMED $NODE_MIN_VERSION" | awk '{print ($1 < $2)}'` == 1 ]];
+        then
+          print_error_message "NODE Version ${NODE_VERSION_TRIMMED} is less than minimum level required of ${NODE_MIN_VERSION}";
+        else
+          echo "OK: Node is at a supported version"
+        fi
+
+      fi
+  else
+      print_error_message "NODE_HOME is empty";
+  fi
+}
+
+validate_node_home_not_empty() {
+  . zowe-variable-utils.sh
+  validate_variable_is_set "NODE_HOME" "${NODE_HOME}"
+}
 
 # TODO - refactor this into shared script?
 # Note requires #ROOT_DIR to be set to use errror.sh, otherwise falls back to stderr
