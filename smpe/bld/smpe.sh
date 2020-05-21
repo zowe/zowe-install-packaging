079772ab97a9903a62105c079fc0af9cb41f211d
John Davies <daviesja@uk.ibm.com>
2020-05-21 16:27:34 +0100

    add filename of converted Ref
    Signed-off-by: John Davies <daviesja@uk.ibm.com>
    Signed-off-by: Jiri Cizek <jiri.cizek@broadcom.com>

--- smpe/bld/smpe.sh
+++ smpe/bld/smpe.sh
@@ -242,7 +242,7 @@ $binDir/zowe-checksum-runtime.sh $stageDir $utilsDir/hash
 mkdir -p $stageDir/fingerprint
 cp   $utilsDir/hash/RefRuntimeHash.txt $stageDir/fingerprint/RefRuntimeHash-$zoweReleaseNumber.txt 
 # convert derived runtime hash file to ASCII and publish on JFrog
-iconv -f IBM-1047 -t ISO8859-1 $utilsDir/hash/RefRuntimeHash.txt > $ROOT/..  # /RefRuntimeHash-$zoweReleaseNumber.txt 
+iconv -f IBM-1047 -t ISO8859-1 $utilsDir/hash/RefRuntimeHash.txt > $ROOT/../RefRuntimeHash.txt 
 
 # save compiled hash program under ROOT_DIR/bin/internal
 cp   $utilsDir/hash/HashFiles.class    $binDir/internal
@@ -254,6 +254,7 @@ cp   $utilsDir/hash/HashFiles.class    $ROOT/.. # for publication on JFrog
 $binDir/zowe-verify-authenticity.sh 
 if [[ $? -ne 0 ]]
 then
+  echo Contents of zowe-verify-authenticity.log 
   cat ~/zowe/fingerprint/*.log  # fragile, because '-l  outputPath' was not specified, so script chose location of log
 fi
 
