f062f805b00a4952389e714593fed6396f7ec603
John Davies <daviesja@uk.ibm.com>
2020-06-02 11:43:27 +0100

    add JAVA_HOME to PATH
    Signed-off-by: John Davies <daviesja@uk.ibm.com>
    Signed-off-by: Jiri Cizek <jiri.cizek@broadcom.com>

--- bin/zowe-verify-authenticity.sh
+++ bin/zowe-verify-authenticity.sh
@@ -252,6 +252,7 @@ fi
 
 echo " ++ Forcing JAVA_HOME ++"
 JAVA_HOME=/usr/lpp/java/J8.0_64 ### THIS MUST BE DONE PROPERLY BY THE AUTOMATION OUTSIDE THIS SCRIPT
+PATH=$PATH:$JAVA_HOME
 echo Info: Checking java version >> $LOG_FILE
 java -version 2>> $LOG_FILE
 if [[ $? -ne 0 ]]
