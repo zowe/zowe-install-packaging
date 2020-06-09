8d368e5028bf4c539f64b8c0664353d98cb362e7
Suneeth Keerthy <skeerthy@rocketsoftware.com>
2020-06-09 17:10:17 -0400

    logs again
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- bin/zowe-setup-certificates.sh
+++ bin/zowe-setup-certificates.sh
@@ -31,6 +31,8 @@ then
   export ZOWE_ROOT_DIR=$(cd $(dirname $0)/../;pwd)
 fi
 
+echo "We get to this file"
+
 . ${ZOWE_ROOT_DIR}/bin/utils/setup-log-dir.sh
 set_install_log_directory "${LOG_DIRECTORY}"
 validate_log_file_not_in_root_dir "${LOG_DIRECTORY}" "${ZOWE_ROOT_DIR}"
