66f11864c6b0110e7dd4297372ce380e0697f180
Suneeth Keerthy <skeerthy@rocketsoftware.com>
2020-06-22 16:11:16 -0400

    -
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- install/zowe-install.sh
+++ install/zowe-install.sh
@@ -10,7 +10,7 @@
 # Copyright IBM Corporation 2018, 2020
 ################################################################################
 
-if [ $# -lt 4 ]; then
+if [ $# -lt 2 ]; then
   echo "Usage: $0 -i <zowe_install_path> -h <zowe_dsn_prefix> [-l <log_directory>]"
   exit 1
 fi
@@ -85,12 +85,14 @@ fi
 echo "Install started at: "`date` >> $LOG_FILE
 
 
-if [[ -z "$DSN_PREFIX" ]]
-then
-  echo "-h parameter not set. Usage: $0 -i zowe_install_path -h zowe_dsn_prefix"
-  exit 1
-else
-  ZOWE_DSN_PREFIX=$DSN_PREFIX
+if [ `uname` = "OS/390" ]; then
+if [[ -z "$DSN_PREFIX" ]]	  if [[ -z "$DSN_PREFIX" ]]
+then	  then
+  echo "-h parameter not set. Usage: $0 -i zowe_install_path -h zowe_dsn_prefix"	    echo "-h parameter not set. Usage: $0 -i zowe_install_path -h zowe_dsn_prefix"
+  exit 1	    exit 1
+else	  else
+  ZOWE_DSN_PREFIX=$DSN_PREFIX	    ZOWE_DSN_PREFIX=$DSN_PREFIX
+  fi
 fi
 
 echo "Beginning install of Zowe ${ZOWE_VERSION} into directory " $ZOWE_ROOT_DIR
@@ -154,8 +156,10 @@ chmod -R 755 $ZOWE_ROOT_DIR/bin
 
 chmod -R 755 $ZOWE_ROOT_DIR/scripts/internal
 
+if [ `uname` = "OS/390" ]; then
 echo "Creating MVS artefacts SZWEAUTH and SZWESAMP" >> $LOG_FILE
 . $INSTALL_DIR/scripts/zowe-install-MVS.sh
+fi
 
 echo "Zowe ${ZOWE_VERSION} runtime install completed into"
 echo "  directory " $ZOWE_ROOT_DIR
