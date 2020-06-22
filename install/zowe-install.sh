f4d4f0804292de29fb64e54ba2b6a9862fa50385
John Davies <daviesja@uk.ibm.com>
2020-06-22 15:44:41 +0100

    remove fingerprint present conf echo
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- install/zowe-install.sh
+++ install/zowe-install.sh
@@ -152,7 +152,6 @@ echo "Copying the run-zowe.sh into "$ZOWE_ROOT_DIR/scripts/internal >> $LOG_FILE
 # if it exists in the INSTALL_DIR driectory
 if [[ -d $INSTALL_DIR/fingerprint ]]
 then
-  echo "OK: Fingerprint exists"
   echo "OK: Fingerprint exists in install directory $INSTALL_DIR and will be copied to runtime" >> $LOG_FILE
   ls -l $INSTALL_DIR/fingerprint/*  >> $LOG_FILE
   mkdir -p  $ZOWE_ROOT_DIR/fingerprint
