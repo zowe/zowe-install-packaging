0821b9ed16beb6a6a8a341dd60cb7f98fe4c2d09
Timothy Gerstel <tim.gerstel@gmail.com>
2020-07-01 16:11:25 -0400

    Add debug statements
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- bin/utils/java-utils.sh
+++ bin/utils/java-utils.sh
@@ -84,11 +84,13 @@ validate_java_home_not_empty() {
 # Given a java version string from the `java -version` command, checks if it is valid
 check_java_version() {
   java_version_output=$1
+  echo "JAVA_VERSION_OUTPUT: ${java_version_output}"
   java_version=$(echo ${java_version_output} | sed -e "s/java version //g"| sed -e "s/\"//g")
   if [[ $java_version = "" ]]; then
     java_version=$(echo ${java_version_output} | sed -e "s/openjdk version //g"| sed -e "s/\"//g")
   fi
-
+  
+  echo "java version parsed: ${java_version}"
   java_major_version=$(echo ${java_version} | cut -d '.' -f 1)
   java_minor_version=$(echo ${java_version} | cut -d '.' -f 2)
 
