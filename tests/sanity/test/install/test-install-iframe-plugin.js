57ae4bcdea25b69f7cc76422dcc36349fdc00a7f
stevenhorsman <steven@uk.ibm.com>
2020-07-22 16:07:55 +0100

    linitng errors
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>
    Signed-off-by: Jiri Cizek <jiri.cizek@broadcom.com>

--- tests/sanity/test/install/test-install-iframe-plugin.js
+++ tests/sanity/test/install/test-install-iframe-plugin.js
@@ -12,7 +12,7 @@ const sshHelper = require('./ssh-helper');
 
 const install_iframe_script='zowe-install-iframe-plugin.sh';
 const install_iframe_path = `${process.env.ZOWE_ROOT_DIR}/bin/utils/${install_iframe_script}`;
-describe.only(`verify ${install_iframe_script}`, function() { //TODO NOW - remove only
+describe(`verify ${install_iframe_script}`, function() {
   before('prepare SSH connection', async function() {
     await sshHelper.prepareConnection();
   });
@@ -27,43 +27,43 @@ describe.only(`verify ${install_iframe_script}`, function() { //TODO NOW - remov
 
     it('test legacy mode still works and defaults version to 1.0.0', async function() {
       const parameters = `"${id}" "${short_name}" "${url}" "${directory}" "${tile_image}"`;
-      expected = get_expected_output(id, short_name, url, directory, tile_image);
+      const expected = get_expected_output(id, short_name, url, directory, tile_image);
       await test_install_iframe_has_expected_rc_stdout_stderr(parameters, 4, expected);
     });
 
     it('test no parameters prints usage correctly', async function() {
       const parameters = '';
-      expected = missing_parameters_message(['i','s','u','d','t']);
+      const expected = missing_parameters_message(['i','s','u','d','t']);
       await test_install_iframe_has_expected_rc_stdout_stderr(parameters, 1, expected);
     });
 
     it('test missing id prints error and usage correctly', async function() {
       const parameters = `-s "${short_name}" -u "${url}" -d "${directory}" -t "${tile_image}"`;
-      expected = missing_parameters_message(['i']);
+      const expected = missing_parameters_message(['i']);
       await test_install_iframe_has_expected_rc_stdout_stderr(parameters, 1, expected);
     });
 
     const getopts_parameters = `-i "${id}" -s "${short_name}" -u "${url}" -d "${directory}" -t "${tile_image}"`;
     it('test getopts mode works and defaults version to 1.0.0', async function() {
-      expected = get_expected_output(id, short_name, url, directory, tile_image);
+      const expected = get_expected_output(id, short_name, url, directory, tile_image);
       await test_install_iframe_has_expected_rc_stdout_stderr(getopts_parameters, 4, expected);
     });
 
     it('test getopts mode works with specified version', async function() {
-      const version = "3.1.4";
-      parameters_with_version = `${getopts_parameters} -v ${version}`;
-      expected = get_expected_output(id, short_name, url, directory, tile_image, version);
+      const version = '3.1.4';
+      const parameters_with_version = `${getopts_parameters} -v ${version}`;
+      const expected = get_expected_output(id, short_name, url, directory, tile_image, version);
       await test_install_iframe_has_expected_rc_stdout_stderr(parameters_with_version, 4, expected);
     });
 
     function missing_parameters_message(missing_parms) {
       const missing_parameters = missing_parms.map(function (flag) {
-        return `-${flag}`
+        return `-${flag}`;
       }).join(' ');
 
       return `Some required parameters were not supplied: ${missing_parameters}
 Usage: ${install_iframe_path} -i <plugin_id> -s <plugin_short_name> -u <url> -d <plugin_directory> -t <tile_image_path> [-v <plugin_version>]
-  eg. ${install_iframe_path} -i "org.zowe.plugin.example" -s "Example plugin" -u "https://zowe.org:443/about-us/" -d "/zowe/component/plugin" -t "/zowe_plugin/artifacts/tile_image.png" -v "1.0.0"`
+  eg. ${install_iframe_path} -i "org.zowe.plugin.example" -s "Example plugin" -u "https://zowe.org:443/about-us/" -d "/zowe/component/plugin" -t "/zowe_plugin/artifacts/tile_image.png" -v "1.0.0"`;
     }
 
     function get_expected_output(id, short_name, url, directory, tile_image, version = "1.0.0") {
@@ -72,7 +72,7 @@ Usage: ${install_iframe_path} -i <plugin_id> -s <plugin_short_name> -u <url> -d 
   });
   
   async function test_install_iframe_has_expected_rc_stdout_stderr(parameters, expected_rc, expected_stdout) {
-    await sshHelper.testCommand(`${install_iframe_path} -z ${parameters}`, expected_rc, expected_stdout, '', exact_match = true);
+    await sshHelper.testCommand(`${install_iframe_path} -z ${parameters}`, expected_rc, expected_stdout, '',  true);
   }
 
   after('dispose SSH connection', function() {
