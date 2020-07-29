761231faf186dda030e6eed7106a6987f3dbc842
OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>
2020-07-29 11:35:00 -0400

    change copyright to IBM & bypass issue 1550
    Signed-off-by: OnnoVdT <49648041+OnnoVdT@users.noreply.github.com>

--- scripts/utils/zowe-install-xmem.sh
+++ scripts/utils/zowe-install-xmem.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright Contributors to the Zowe Project. 2019, 2020
+# Copyright IBM Corporation 2019, 2020
 #######################################################################
 
 # Function: Copy cross-memory server artefacts to the required locations
