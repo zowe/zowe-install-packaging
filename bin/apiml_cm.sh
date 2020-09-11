c2be3b08a754b1f27ad52a78ec393ffefcf025c4
Timothy Gerstel <tgerstel@rocketsoftware.com>
2020-09-11 14:04:10 -0400

    -s includes -e
    Signed-off-by: Timothy Gerstel <tgerstel@rocketsoftware.com>

--- bin/apiml_cm.sh
+++ bin/apiml_cm.sh
@@ -551,7 +551,6 @@ function trust_zosmf {
     csplit -s -k -f ${CER_DIR}/${ALIAS} ${CER_DIR}/${TEMP_CERT_FILE} /-----END\ CERTIFICATE-----/1 \
       {$(expr `grep -c -e '-----END CERTIFICATE-----' ${CER_DIR}/${TEMP_CERT_FILE}` - 1)}
     for entry in ${CER_DIR}/${ALIAS}*; do
-      [ -e "$entry" ] || continue
       [ -s "$entry" ] || continue
       CERTIFICATE=${entry}
       entry=${entry##*/}
