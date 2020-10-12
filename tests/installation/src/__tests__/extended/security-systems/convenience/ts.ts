827973c8d5e565099ce9b0aebda30d5ca8f999bb
achmelo <a.chmelo@gmail.com>
2020-10-12 11:17:18 +0200

    choose correct security system
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- tests/installation/src/__tests__/extended/security-systems/convenience/ts.ts
+++ tests/installation/src/__tests__/extended/security-systems/convenience/ts.ts
@@ -13,7 +13,7 @@ import {
   installAndVerifyConvenienceBuild,
   showZoweRuntimeLogs,
 } from '../../../../utils';
-import { TEST_TIMEOUT_CONVENIENCE_BUILD } from '../../../../constants';
+import {SECURITY_SYSTEM_TSS, TEST_TIMEOUT_CONVENIENCE_BUILD} from '../../../../constants';
 
 /**
  * Define this test should run in a specific worker
@@ -37,6 +37,7 @@ describe(testSuiteName, () => {
       testServer,
       {
         'zowe_build_local': process.env['ZOWE_BUILD_LOCAL'],
+        'zos_security_system': SECURITY_SYSTEM_TSS,
       }
     );
   }, TEST_TIMEOUT_CONVENIENCE_BUILD);
