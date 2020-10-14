fa3737ee8d74cb7ca8bf8e71921f351d028b5721
achmelo <a.chmelo@gmail.com>
2020-10-14 15:13:53 +0200

    lock keystore conditionaly
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- bin/zowe-setup-certificates.sh
+++ bin/zowe-setup-certificates.sh
@@ -323,21 +323,22 @@ EOF
 fi
 
 # set up privileges and ownership
-chmod -R 500 ${KEYSTORE_DIRECTORY}/${LOCAL_KEYSTORE_SUBDIR}/* ${KEYSTORE_DIRECTORY}/${KEYSTORE_ALIAS}/* 2> /dev/null # In some keystore scenarios these directories might be empty, so supress error
-echo "Trying to change an owner of the ${KEYSTORE_DIRECTORY}."
-if ! chown -R ${ZOWE_USER_ID} ${KEYSTORE_DIRECTORY} >> $LOG_FILE 2>&1 ; then
-  echo "Unable to change the current owner of the ${KEYSTORE_DIRECTORY} directory to the ${ZOWE_USER_ID} owner. See $LOG_FILE for more details."
-  echo "Trying to change a group of the ${KEYSTORE_DIRECTORY}."
-  chmod -R 550 ${KEYSTORE_DIRECTORY}
-  if ! chgrp -R ${ZOWE_GROUP_ID} ${KEYSTORE_DIRECTORY} >> $LOG_FILE 2>&1 ; then
-    echo "Unable to change the group of the ${KEYSTORE_DIRECTORY} directory to the ${ZOWE_GROUP_ID} group. See $LOG_FILE for more details."
-    echo "Please change the owner or the group of the ${KEYSTORE_DIRECTORY} manually so that keystores are protected correctly!"
-    echo "Ideally, only ${ZOWE_USER_ID} should have access to the ${KEYSTORE_DIRECTORY}."
+if [${ZOWE_LOCK_KEYSTORE} = true] ; then
+  chmod -R 500 ${KEYSTORE_DIRECTORY}/${LOCAL_KEYSTORE_SUBDIR}/* ${KEYSTORE_DIRECTORY}/${KEYSTORE_ALIAS}/* 2> /dev/null # In some keystore scenarios these directories might be empty, so supress error
+  echo "Trying to change an owner of the ${KEYSTORE_DIRECTORY}."
+  if ! chown -R ${ZOWE_USER_ID} ${KEYSTORE_DIRECTORY} >> $LOG_FILE 2>&1 ; then
+    echo "Unable to change the current owner of the ${KEYSTORE_DIRECTORY} directory to the ${ZOWE_USER_ID} owner. See $LOG_FILE for more details."
+    echo "Trying to change a group of the ${KEYSTORE_DIRECTORY}."
+    chmod -R 550 ${KEYSTORE_DIRECTORY}
+    if ! chgrp -R ${ZOWE_GROUP_ID} ${KEYSTORE_DIRECTORY} >> $LOG_FILE 2>&1 ; then
+      echo "Unable to change the group of the ${KEYSTORE_DIRECTORY} directory to the ${ZOWE_GROUP_ID} group. See $LOG_FILE for more details."
+      echo "Please change the owner or the group of the ${KEYSTORE_DIRECTORY} manually so that keystores are protected correctly!"
+      echo "Ideally, only ${ZOWE_USER_ID} should have access to the ${KEYSTORE_DIRECTORY}."
+    else
+      echo "Group of the ${KEYSTORE_DIRECTORY} changed successfully to the ${ZOWE_GROUP_ID} owner."
+    fi
   else
-    echo "Group of the ${KEYSTORE_DIRECTORY} changed successfully to the ${ZOWE_GROUP_ID} owner."
+    echo "Owner of the ${KEYSTORE_DIRECTORY} changed successfully to the ${ZOWE_USER_ID} owner."
   fi
-else
-  echo "Owner of the ${KEYSTORE_DIRECTORY} changed successfully to the ${ZOWE_USER_ID} owner."
 fi
-
 echo "</zowe-setup-certificates.sh>" >> $LOG_FILE
