1a58291c142a357c71cfefde191e3b150cb92636
achmelo <a.chmelo@gmail.com>
2020-10-30 18:26:25 +0100

    change label
    Signed-off-by: achmelo <a.chmelo@gmail.com>

--- tests/installation/src/__tests__/basic/install-api-gen.ts
+++ tests/installation/src/__tests__/basic/install-api-gen.ts
@@ -32,7 +32,7 @@ import {
           'zowe_build_local': process.env['ZOWE_BUILD_LOCAL'],
           'zowe_lock_keystore': 'false',
           'zowe_token_name': 'ZWETOKEN',
-          'zowe_token_label': 'ZWLZSSBL',
+          'zowe_token_label': 'SOZWLBLE',
         }
       );
     }, TEST_TIMEOUT_CONVENIENCE_BUILD);
