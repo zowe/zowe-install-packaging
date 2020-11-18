3acec00a491f3590ece405b963cbc56a772e4f2b
stevenhorsman <steven@uk.ibm.com>
2020-11-18 14:05:49 +0000

    Update explorer ui server install
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/zowe-install-explorer-ui.sh
+++ scripts/zowe-install-explorer-ui.sh
@@ -18,40 +18,30 @@
 
 echo "<zowe-explorer-ui-install.sh>" >> $LOG_FILE
 
-# install explorer-ui-server
-cd $INSTALL_DIR
-EXPLORER_SERVER_PAX=$PWD/$(ls -t ./files/explorer-ui-server-*.pax | head -1)
-EXPLORER_INSTALL_FOLDER=${ZOWE_ROOT_DIR}/shared/explorer-ui-server
-echo "  Installing explorer-ui-server into ${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
 umask 0002
-mkdir -p "${EXPLORER_INSTALL_FOLDER}"
-cd ${EXPLORER_INSTALL_FOLDER}
-echo "  Unpax of ${EXPLORER_SERVER_PAX} into ${PWD}" >> $LOG_FILE
-pax -rf ${EXPLORER_SERVER_PAX} -ppx
-chmod -R 755 .
-
-UI_PLUGIN_LIST="explorer-jes explorer-mvs explorer-uss"
-for component_id in $UI_PLUGIN_LIST; do
-  cd $INSTALL_DIR
-  EXPLORER_PLUGIN_PAX=$PWD/$(ls -t ./files/${component_id}-*.pax | head -1)
-  if [ ! -f $EXPLORER_PLUGIN_PAX ]; then
+ui_components="explorer-ui-server explorer-jes explorer-mvs explorer-uss"
+for component_id in ${ui_components}; do
+  cd ${INSTALL_DIR}
+  component_pax=$PWD/$(ls -t ./files/${component_id}-*.pax | head -1)
+  if [ ! -f ${component_pax} ]; then
     echo "  ${component_id} Explorer UI (${component_id}-*.pax) missing"
     echo "  Installation terminated"
     exit 0
   fi
 
   # NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-  EXPLORER_INSTALL_FOLDER="${ZOWE_ROOT_DIR}/components/${component_id}"
-  echo "  Installing Explorer UI ${component_id} into ${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
-  umask 0002
-  mkdir -p "${EXPLORER_INSTALL_FOLDER}/bin"
+  component_directory="${ZOWE_ROOT_DIR}/components/${component_id}"
+  echo "  Installing Explorer UI ${component_id} into ${component_directory} ..."  >> $LOG_FILE
   
-  # unpax package
-  cd "${EXPLORER_INSTALL_FOLDER}"
-  echo "  Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
-  pax -rf $EXPLORER_PLUGIN_PAX -ppx
+  mkdir -p "${component_directory}"
+  echo "  Unpax of ${component_pax} into ${PWD}" >> $LOG_FILE
+  pax -rf ${component_pax} -ppx
 
-  chmod -R 755 "${ZOWE_ROOT_DIR}/components/explorer-${COMPONENT_ID}/bin"
+  # TODO - do we need this section. Can we unify it, or derive this from the manifest?
+  if [[ "${component_id}" == "explorer-ui-server " ]]; then
+    chmod -R 755 "${component_directory}"
+  elif
+    chmod -R 755 "${component_directory}/bin"
+  fi
 done
-
 echo "</zowe-explorer-ui-install.sh>" >> $LOG_FILE
