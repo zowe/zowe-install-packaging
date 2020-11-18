0bd307a26dd75f73088952fabbb024a251f35c08
stevenhorsman <steven@uk.ibm.com>
2020-11-18 12:32:26 +0000

    Update manifest and explorer install script
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/zowe-install-explorer-ui.sh
+++ scripts/zowe-install-explorer-ui.sh
@@ -30,62 +30,26 @@ echo "  Unpax of ${EXPLORER_SERVER_PAX} into ${PWD}" >> $LOG_FILE
 pax -rf ${EXPLORER_SERVER_PAX} -ppx
 chmod -R 755 .
 
-UI_PLUGIN_LIST="jes mvs uss"
-for COMPONENT_ID in $UI_PLUGIN_LIST; do
-  EXPLORER_PLUGIN_UPPERCASE=$(echo $COMPONENT_ID | tr '[a-z]' '[A-Z]')
-
+UI_PLUGIN_LIST="explorer-jes explorer-mvs explorer-uss"
+for component_id in $UI_PLUGIN_LIST; do
   cd $INSTALL_DIR
-  EXPLORER_PLUGIN_PAX=$PWD/$(ls -t ./files/explorer-${COMPONENT_ID}-*.pax | head -1)
+  EXPLORER_PLUGIN_PAX=$PWD/$(ls -t ./files/${component_id}-*.pax | head -1)
   if [ ! -f $EXPLORER_PLUGIN_PAX ]; then
-    echo "  ${EXPLORER_PLUGIN_UPPERCASE} Explorer UI (explorer-${COMPONENT_ID}-*.pax) missing"
+    echo "  ${component_id} Explorer UI (${component_id}-*.pax) missing"
     echo "  Installation terminated"
     exit 0
   fi
 
   # NOTICE: zowe-install-iframe-plugin.sh will try to automatically create install folder based on plugin name
-  EXPLORER_INSTALL_FOLDER="${ZOWE_ROOT_DIR}/components/explorer-${COMPONENT_ID}"
-  echo "  Installing Explorer UI ${EXPLORER_PLUGIN_UPPERCASE} into ${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
+  EXPLORER_INSTALL_FOLDER="${ZOWE_ROOT_DIR}/components/${component_id}"
+  echo "  Installing Explorer UI ${component_id} into ${EXPLORER_INSTALL_FOLDER} ..."  >> $LOG_FILE
   umask 0002
   mkdir -p "${EXPLORER_INSTALL_FOLDER}/bin"
   
-  if [[ "${COMPONENT_ID}" == "jes" ]]; then
-    # unpax package
-    cd "${EXPLORER_INSTALL_FOLDER}"
-    echo "  Alt: Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
-    pax -rf $EXPLORER_PLUGIN_PAX -ppx
-  fi
-
-  if [[ "${COMPONENT_ID}" != "jes" ]]; then
-      # unpax package
-    cd "${EXPLORER_INSTALL_FOLDER}/bin"
-    echo "  Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
-    pax -rf $EXPLORER_PLUGIN_PAX -ppx
-
-    EXPLORER_UI_START_SCRIPT=$EXPLORER_INSTALL_FOLDER/bin/scripts/explorer-${COMPONENT_ID}-start.sh
-    EXPLORER_UI_CONFIGURE_SCRIPT=$EXPLORER_INSTALL_FOLDER/bin/scripts/explorer-${COMPONENT_ID}-configure.sh
-    EXPLORER_UI_VALIDATE_SCRIPT=$EXPLORER_INSTALL_FOLDER/bin/scripts/explorer-${COMPONENT_ID}-validate.sh
-
-    if [ ! -f $EXPLORER_UI_START_SCRIPT ]; then
-      echo "  Error: Explorer ${COMPONENT_ID} ui start script (start-explorer-${COMPONENT_ID}-ui-server.sh) missing"
-      echo "  Installation terminated"
-      exit 0
-    fi
-
-    # copy start script
-    mv ${EXPLORER_UI_START_SCRIPT} start.sh
-
-    if [[ -f ${EXPLORER_UI_CONFIGURE_SCRIPT} ]]
-    then
-      mv ${EXPLORER_UI_CONFIGURE_SCRIPT} configure.sh
-    fi
-
-    if [[ -f ${EXPLORER_UI_VALIDATE_SCRIPT} ]]
-    then
-      mv ${EXPLORER_UI_VALIDATE_SCRIPT} validate.sh
-    fi
-
-    rm -rf $EXPLORER_INSTALL_FOLDER/bin/scripts
-  fi
+  # unpax package
+  cd "${EXPLORER_INSTALL_FOLDER}"
+  echo "  Unpax of ${EXPLORER_PLUGIN_PAX} into ${PWD}" >> $LOG_FILE
+  pax -rf $EXPLORER_PLUGIN_PAX -ppx
 
   chmod -R 755 "${ZOWE_ROOT_DIR}/components/explorer-${COMPONENT_ID}/bin"
 done
