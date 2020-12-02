ef6060327427af3091e522e017e3732168d2ab1f
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-12-02 15:21:40 -0500

    Update with ability to show only low verbosity logs
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/internal/run-zowe.sh
+++ bin/internal/run-zowe.sh
@@ -35,6 +35,51 @@ export ROOT_DIR=$(cd $(dirname $0)/../../;pwd)
 launch_components_list=$(${ROOT_DIR}/bin/internal/get-launch-components.sh -c "${INSTANCE_DIR}")
 . ${ROOT_DIR}/bin/internal/prepare-workspace.sh -c "${INSTANCE_DIR}" -t "${launch_components_list}"
 
+# FIXME: Zowe Launcher should probably do this file logging logic instead
+# Handle log file syntax setup here so that the timestamps for all components match
+# Determine if components should log to a file, and where
+if [ -z "$ZWE_NO_LOGFILE" ]
+then
+  if [ -z "$ZWE_LOG_DIR" ]
+  then
+    export ZWE_LOG_DIR=${INSTANCE_DIR}/logs
+  fi
+  if [ -f "$ZWE_LOG_DIR" ]
+  then
+    export ZWE_NO_LOGFILE=1
+  elif [ ! -d "$ZWE_LOG_DIR" ]
+  then
+    echo "Will make log directory $ZWE_LOG_DIR"
+    mkdir -p $ZWE_LOG_DIR
+    if [ $? -ne 0 ]
+    then
+      echo "Cannot make log directory.  Logging disabled."
+      export ZWE_NO_LOGFILE=1
+    fi
+  fi
+  export ZWE_ROTATE_LOGS=0
+  if [ -d "$ZWE_LOG_DIR" ]
+  then
+    LOG_SUFFIX="`date +%Y-%m-%d-%H-%M`.log"
+    if [ -z "$ZWE_LOGS_TO_KEEP" ]
+    then
+      export ZWE_LOGS_TO_KEEP=5
+    fi
+    echo $ZWE_LOGS_TO_KEEP|egrep '^\-?[0-9]+$' >/dev/null
+    if [ $? -ne 0 ]
+    then
+      echo "ZWE_LOGS_TO_KEEP not a number.  Defaulting to 5."
+      export ZWE_LOGS_TO_KEEP=5
+    fi
+    if [ $ZWE_LOGS_TO_KEEP -ge 0 ]
+    then
+      export ZWE_ROTATE_LOGS=1
+    fi
+  fi
+fi
+
+
+
 # FIXME: Zowe Launcher can take responsibility from here
 for component_id in $(echo "${launch_components_list}" | sed "s/,/ /g"); do
   . ${ROOT_DIR}/bin/internal/start-component.sh -c "${INSTANCE_DIR}" -o "${component_id}" &
--- bin/internal/start-component.sh
+++ bin/internal/start-component.sh
@@ -51,6 +51,56 @@ component_dir=$(find_component_directory "${component_id}")
 export LAUNCH_COMPONENT="${component_dir}/bin"
 # FIXME: change here to read manifest `commands.start` entry
 start_script=${component_dir}/bin/start.sh
+
 if [ ! -z "${component_dir}" -a -x "${start_script}" ]; then
-  . ${start_script}
+  COMPONENT_NAME=$(cd ${component_id}/../ && echo "${PWD##*/}")
+  if [ "${COMPONENT_NAME}" == "zss"]
+  then
+    COMPONENT_NAME="zssServer" #backwards compatibility
+  elif [ "${COMPONENT_NAME}" == "app-server"]
+  then
+    COMPONENT_NAME="appServer" #backwards compatibility
+  fi
+
+  ZWE_LOG_FILE=${ZWE_LOG_DIR}/${COMPONENT_NAME}-${LOG_SUFFIX}
+  if [ -z $ZWE_NO_LOGFILE ]
+  then
+    if [ ! -e "$ZWE_LOG_FILE" ]
+    then
+      touch $ZWE_LOG_FILE
+      if [ $? -ne 0 ]
+      then
+        echo "Cannot make log file '$ZWE_LOG_FILE'.  Logging disabled."
+        ZWE_NO_LOGFILE=1
+      fi
+    else
+      if [ -d "$ZWE_LOG_FILE" ]
+      then
+        echo "ZWE_LOG_FILE '$ZWE_LOG_FILE' is a directory.  Must be a file.  Logging disabled."
+        ZWE_NO_LOGFILE=1
+      fi
+    fi
+    if [ ! -w "$ZWE_LOG_FILE" ]
+    then
+      echo "file '$ZWE_LOG_FILE' is not writable. Logging disabled."
+      ZWE_NO_LOGFILE=1
+    fi
+  fi
+  if [ -z "$ZWE_NO_LOGFILE" ]
+  then
+    #Clean up excess logs, if appropriate.
+    if [ $ZWE_ROTATE_LOGS -ne 0 ]
+    then
+      for f in `ls -r -1 $ZWE_LOG_DIR/${component_id}-*.log 2>/dev/null | tail +$ZWE_LOGS_TO_KEEP`
+      do
+        echo "${component_id} removing old log file '$f'"
+        rm -f $f
+      done
+    fi
+    echo "${component_id} log file=${ZWE_LOG_FILE}"
+    . ${start_script} 2>&1 | tee ${ZWE_LOG_FILE} | grep -E "(INFO|WARN|CRITICAL|ERROR)"
+  else
+    echo "${component_id} not logging to a file"
+    . ${start_script}
+  fi
 fi
