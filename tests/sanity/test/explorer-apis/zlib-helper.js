2ad8acd6526c52894daba335c3161093d8eeae6e
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-01-11 11:55:39 -0500

    add manual compression to check gzip content-encoding header
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- tests/sanity/test/explorer-apis/zlib-helper.js
+++ tests/sanity/test/explorer-apis/zlib-helper.js
@@ -1,12 +1,15 @@
+const _ = require('lodash');
 const zlib = require('zlib');
-
+const expect = require('chai').expect;
 
 const deflate = (dataStream) => new Promise((resolve, reject) => {
   try {
     let gunzip = zlib.createGunzip();            
     dataStream.pipe(gunzip);
     gunzip.on('data', function(data) {
-      resolve(JSON.parse(data.toString()));
+      let dataStr = data.toString();
+      // content JSON.parse(data.toString()) parsing can cause exception
+      resolve(JSON.parse(dataStr));
     });
     gunzip.on('error', function(err) {
       reject(err);
@@ -16,6 +19,47 @@ const deflate = (dataStream) => new Promise((resolve, reject) => {
   }
 });
 
+function manualDecompressionRequest(REQ, req, ungzip) {
+  let overrideOptions = {
+    responseType: 'stream',
+    decompress: false
+  };
+
+  let addHeader = {
+    'Accept-Encoding': 'gzip'
+  };
+
+  req = Object.assign({},req,overrideOptions);
+  req.headers = Object.assign({},req.headers,addHeader);
+  
+
+  let resp;
+  return REQ.request(req)
+    .then(function(res) {
+      expect(res.headers).to.have.property('content-encoding');
+      expect(res.headers['content-encoding']).to.equal('gzip');  
+      resp = _.pick(res, ['status', 'statusText', 'headers', 'data']);
+      if(ungzip) {
+        return deflate(res.data);  
+      } else {
+        return res.data;
+      }
+    })
+    .then((data) => {
+      resp.data = data;
+      return resp;
+    });
+}
+
+function handleCompressionRequest(REQ, req, handleOptions) {
+  let options =  Object.assign({},{manualDecompress: false, ungzip: true}, handleOptions);
+  if(options.manualDecompress) {
+    return manualDecompressionRequest(REQ,req, options.ungzip);
+  }
+  return REQ.request(req);
+}
+
 module.exports = {
-  deflate
+  deflate,
+  handleCompressionRequest
 };
