5192f3743e5ab2d9cc46542b0ddba93ba2764969
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-01-18 11:31:42 -0500

    try v12.18.4 instead of v12.16.1
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- tests/installation/src/__tests__/extended/node-versions/node-v12.ts
+++ tests/installation/src/__tests__/extended/node-versions/node-v12.ts
@@ -32,7 +32,7 @@ describe(testSuiteName, () => {
       testServer,
       {
         'zowe_build_local': process.env['ZOWE_BUILD_LOCAL'],
-        'zos_node_home': '/ZOWE/node/node-v12.16.1-os390-s390x',
+        'zos_node_home': '/ZOWE/node/node-v12.18.4-os390-s390x',
         'zowe_lock_keystore': 'false',
       }
     );
