7891df71071faed2ee55001b4cf438d82b64dbcc
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-03-01 10:42:10 +0500

    Add exit 0
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- bin/internal/prepare-workspace-for-launcher.sh
+++ bin/internal/prepare-workspace-for-launcher.sh
@@ -34,3 +34,5 @@ export ROOT_DIR
 
 launch_components_list=$(${ROOT_DIR}/bin/internal/get-launch-components.sh -c "${INSTANCE_DIR}" -r "${ROOT_DIR}")
 . ${ROOT_DIR}/bin/internal/prepare-workspace.sh -c "${INSTANCE_DIR}" -r "${ROOT_DIR}" -t "${launch_components_list}"
+
+exit 0
