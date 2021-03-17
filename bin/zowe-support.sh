2a683cd25fcf3fb079a86ca48cabeeeb339fcc31
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-03-16 21:21:33 -0400

    fix zowe-support.sh
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/zowe-support.sh
+++ bin/zowe-support.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Copyright IBM Corporation 2019, 2020
+# Copyright IBM Corporation 2019, 2021
 ################################################################################
 
 while getopts "l:" opt; do
@@ -39,6 +39,10 @@ VERSION_FILE=${SUPPORT_ARCHIVE_LOCATION}"/version_output"
 
 ZOWE_STC=${ZOWE_PREFIX}${ZOWE_INSTANCE}SV
 
+FILE_PATTERNS_COLLECTED=
+
+export TEMP_DIR=${TMPDIR:-/tmp}
+
 function psgrep {
     pattern=[^]]${1};
     # [^]] used to concatenate a static string to the pattern. Done to remove grep command from output
@@ -46,7 +50,7 @@ function psgrep {
 }
 
 function write_to_log {
-    echo $1 | tee -a ${SUPPORT_ARCHIVE_LOG}
+    echo "$1" | tee -a ${SUPPORT_ARCHIVE_LOG}
 }
 
 function add_to_pax {
@@ -67,20 +71,49 @@ function add_to_pax {
             SUBSTITUTION=""
         ;;
     esac
-    pax -wva -o saveext ${SUBSTITUTION} -s#${ROOT_DIR}#ROOT_DIR# -s#${INSTANCE_DIR}#INSTANCE_DIR# -s#${INSTALL_LOG_DIR}#INSTALL_LOG_DIR# -s#${KEYSTORE_DIRECTORY}#KEYSTORE_DIR# -s#${TMPDIR:-/tmp}#TEMP_DIR# -f ${SUPPORT_ARCHIVE}  $1 2>&1 | tee -a ${SUPPORT_ARCHIVE_LOG}
+    pax -wva -o saveext \
+      ${SUBSTITUTION} \
+      "-s#${ROOT_DIR}#ROOT_DIR#" \
+      "-s#${INSTANCE_DIR}#INSTANCE_DIR#" \
+      "-s#${INSTALL_LOG_DIR}#INSTALL_LOG_DIR#" \
+      "-s#${KEYSTORE_DIRECTORY}#KEYSTORE_DIR#" \
+      "-s#${TMPDIR:-/tmp}#TEMP_DIR#" \
+      -f ${SUPPORT_ARCHIVE} \
+      $1 2>&1 | tee -a ${SUPPORT_ARCHIVE_LOG}
 }
 
-function add_file_to_pax_if_found {
-    FILE_PATH=$1
-    if [[ -f ${FILE_PATH} ]];then
-        write_to_log "Collecting ${FILE_PATH}"
-        add_to_pax ${FILE_PATH}
-    else
-        write_to_log "File ${FILE_PATH} was not found."
-    fi
+function add_files_by_pattern_to_pax {
+  FILE_DESCRIPTION=$1
+  FILE_PATH=$2
+  FILE_PATTERN=$3
+
+  if [ -d "${FILE_PATH}" ]; then
+    write_to_log "Collecting ${FILE_DESCRIPTION} (${FILE_PATTERN}) from ${FILE_PATH}"
+    for file in $(find "${FILE_PATH}" -name "${FILE_PATTERN}" -type f) ; do
+      FILE_PATTERNS_COLLECTED="${FILE_PATTERNS_COLLECTED} $file"
+    done
+  fi
 }
+
+function update_pax {
+  pax -wva -o saveext \
+    "-s#${ROOT_DIR}#ROOT_DIR#" \
+    "-s#${INSTANCE_DIR}#INSTANCE_DIR#" \
+    "-s#${INSTALL_LOG_DIR}#INSTALL_LOG_DIR#" \
+    "-s#${KEYSTORE_DIRECTORY}#KEYSTORE_DIR#" \
+    "-s#${TMPDIR:-/tmp}#TEMP_DIR#" \
+    -f ${SUPPORT_ARCHIVE} \
+    ${FILE_PATTERNS_COLLECTED} 2>&1 | tee -a ${SUPPORT_ARCHIVE_LOG}
+}
+
+write_to_log "============================================================================"
+write_to_log "=                           Zowe Support Script                            ="
+write_to_log ""
+write_to_log "Started at ${DATE}"
+write_to_log ""
 
 # Collecting software versions
+write_to_log "----------------------------------------------------------------------------"
 write_to_log "Collecting version of z/OS, Java, NodeJS"
 ZOS_VERSION=`${ROOT_DIR}/scripts/internal/opercmd "D IPLINFO" | grep -i release | xargs`
 write_to_log "  - z/OS: $ZOS_VERSION"
@@ -93,83 +126,87 @@ echo "Java version: "$JAVA_VERSION >> $VERSION_FILE
 echo "NodeJS version: "$NODE_VERSION >> $VERSION_FILE
 add_to_pax $VERSION_FILE version_file
 rm $VERSION_FILE
+write_to_log ""
 
 # Collect process information
+write_to_log "----------------------------------------------------------------------------"
 write_to_log "Collecting current process information based on the following prefix: ${ZOWE_PREFIX}$ZOWE_INSTANCE"
 psgrep $ZOWE_PREFIX$ZOWE_INSTANCE > $PS_OUTPUT_FILE
 write_to_log "Adding ${PS_OUTPUT_FILE}"
 add_to_pax $PS_OUTPUT_FILE process_info
 rm $PS_OUTPUT_FILE
+write_to_log ""
 
 # Collect STC output
-export TEMP_DIR=${TMPDIR:-/tmp}
-
+write_to_log "----------------------------------------------------------------------------"
+write_to_log "Collecting STC output"
 tsocmd status ${ZOWE_STC} 2>/dev/null | \
     sed -n 's/.*JOB *\([^ ]*\)(\([^ ]*\)) ON OUTPUT QUEUE.*/\1 \2/p' > ${TEMP_DIR}/jobname.jobid.$$.list
 while read jobname jobid
 do
-    write_to_log "Collecting output for Zowe started task $jobname($jobid)"
-    STC_FILE=${TEMP_DIR}/$jobname-$jobid.log   # print-joblog-to-file.sh will create a file of this name    
-    ${ROOT_DIR}/bin/utils/print-joblog-to-file.sh $jobname $jobid $STC_FILE | tee -a ${SUPPORT_ARCHIVE_LOG}
-    write_to_log "Return code from print-joblog-to-file.sh was $?"
-    add_to_pax $STC_FILE
+  write_to_log "Collecting output for Zowe started task $jobname($jobid)"
+  STC_FILE=${TEMP_DIR}/$jobname-$jobid.log   # print-joblog-to-file.sh will create a file of this name    
+  ${ROOT_DIR}/bin/utils/print-joblog-to-file.sh $jobname $jobid $STC_FILE | tee -a ${SUPPORT_ARCHIVE_LOG}
+  write_to_log "Return code from print-joblog-to-file.sh was $?"
+  if [ -f "${$STC_FILE}" ]; then
+    add_files_by_pattern_to_pax "STC log file" "${TEMP_DIR}" "${jobname}-${jobid}.log"
     rm $STC_FILE
+  else
+    write_to_log "${STC_FILE} is not created"
+  fi
 done < ${TEMP_DIR}/jobname.jobid.$$.list
 rm     ${TEMP_DIR}/jobname.jobid.$$.list
+write_to_log ""
 
 # Collect install logs
-if [[ -d ${INSTALL_LOG_DIR} ]];then
-    write_to_log "Collecting installation log files from ${INSTALL_LOG_DIR}:"
-    add_to_pax ${INSTALL_LOG_DIR} installation_log *.log
-else
-    write_to_log "Directory ${INSTALL_LOG_DIR} was not found."
-fi
+write_to_log "----------------------------------------------------------------------------"
+add_files_by_pattern_to_pax "installation log files" "${INSTALL_LOG_DIR}" "*"
+write_to_log ""
 
-# Collect rest of logs
-if [[ -d ${RUNTIME_LOG_DIR} ]];then
-    write_to_log "Collecting instance log files from ${RUNTIME_LOG_DIR}:"
-    add_to_pax ${RUNTIME_LOG_DIR} instance_logs *.log
-else
-    write_to_log "Directory ${RUNTIME_LOG_DIR} was not found."
-fi
+# Collect configs
+write_to_log "----------------------------------------------------------------------------"
+add_files_by_pattern_to_pax "Zowe manifest" "$ROOT_DIR" "manifest.json"
+add_files_by_pattern_to_pax "env configs" "${INSTANCE_DIR}" "instance.env"
+add_files_by_pattern_to_pax "YAML configs" "${INSTANCE_DIR}" "zowe.yaml"
+add_files_by_pattern_to_pax "temporary configuration files" "${INSTANCE_DIR}/.env" '*'
+add_files_by_pattern_to_pax "keystore env configs" "${KEYSTORE_DIRECTORY}" "zowe-*.env"
+add_files_by_pattern_to_pax "keystore YAML configs" "${KEYSTORE_DIRECTORY}" "*.yaml"
+write_to_log ""
 
 # Collect api-definitions
+write_to_log "----------------------------------------------------------------------------"
 API_DEFS_DIRECTORY="${INSTANCE_DIR}/workspace/api-mediation/api-defs"
-if [[ -d ${API_DEFS_DIRECTORY} ]];then
-    write_to_log "Collecting instance static definition files from ${API_DEFS_DIRECTORY}:"
-    add_to_pax ${API_DEFS_DIRECTORY} api-defs *.yml
-else
-    write_to_log "Directory ${API_DEFS_DIRECTORY} was not found."
-fi
+add_files_by_pattern_to_pax "instance static definition files" "${API_DEFS_DIRECTORY}" "*.yml"
+write_to_log ""
 
 if [[ -d $ROOT_DIR/fingerprint ]]
 then 
-    write_to_log "Collecting fingerprint"
-    for RefRuntimeHash in `ls ${ROOT_DIR}/fingerprint`
-    do
-        add_file_to_pax_if_found "${ROOT_DIR}/fingerprint/$RefRuntimeHash"
-    done 
-    $ROOT_DIR/bin/zowe-verify-authenticity.sh -l $SUPPORT_ARCHIVE_LOCATION
-    echo SUPPORT_ARCHIVE_LOCATION
-    ls ${SUPPORT_ARCHIVE_LOCATION}
-    for logFileName in `ls ${SUPPORT_ARCHIVE_LOCATION}/zowe-verify-authenticity-*.log`
-    do
-        add_file_to_pax_if_found "${logFileName}"
-    done
+  write_to_log "----------------------------------------------------------------------------"
+  write_to_log "Collecting fingerprint"
+  add_files_by_pattern_to_pax "reference runtime hash" "${ROOT_DIR}/fingerprint" "*"
+  $ROOT_DIR/bin/zowe-verify-authenticity.sh -l $SUPPORT_ARCHIVE_LOCATION
+  add_files_by_pattern_to_pax "customer runtime hash" "${SUPPORT_ARCHIVE_LOCATION}" "CustRuntimeHash.*"
+  add_files_by_pattern_to_pax "customer runtime hash" "${SUPPORT_ARCHIVE_LOCATION}" "RefRuntimeHash.*"
+  write_to_log ""
 fi
 
+# Collect instance logs
+write_to_log "----------------------------------------------------------------------------"
+add_files_by_pattern_to_pax "instance log files" "${RUNTIME_LOG_DIR}" "*.log"
+write_to_log ""
+
 # TODO - collect all the rest of workspace directory?
-add_file_to_pax_if_found "${INSTANCE_DIR}/instance.env"
-add_file_to_pax_if_found "${KEYSTORE_DIRECTORY}/zowe-certificates.env"
-add_file_to_pax_if_found "$ROOT_DIR/manifest.json"
 
-# Add support log file to pax
-write_to_log "Adding ${SUPPORT_ARCHIVE_LOG}"
-add_to_pax ${SUPPORT_ARCHIVE_LOG} support_archive_log
-rm ${SUPPORT_ARCHIVE_LOG}
-
+write_to_log "----------------------------------------------------------------------------"
+write_to_log "Write pax file"
+update_pax
 # Compress pax file
 compress ${SUPPORT_ARCHIVE}
+write_to_log ""
+
+# Clean up
+rm -f ${SUPPORT_ARCHIVE_LOG}
 
 # Print final message
+echo "============================================================================"
 echo "The support file was created ${SUPPORT_ARCHIVE}.Z"
