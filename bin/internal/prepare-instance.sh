0fd0005fa45f98e1e2a13b3455c77bcf4790f682
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-05-11 13:49:19 -0400

    fix missing variables
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/internal/prepare-instance.sh
+++ bin/internal/prepare-instance.sh
@@ -71,22 +71,22 @@ global_validate() {
   validate_directory_is_accessible "${KEYSTORE_DIRECTORY}"
 
   # ZOWE_PREFIX shouldn't be too long
-  validate_zowe_prefix 2>&1 | print_formatted_info "${LOGGING_SERVICE_ID}" "${LOGGING_SCRIPT_NAME}:${LINENO}" -
+  validate_zowe_prefix 2>&1 | print_formatted_info "ZWELS" "prepare-instance.sh,global_validate:${LINENO}" -
 
   # currently node is always required
   # otherwise we should check if these services are starting:
   # - explorer-mvs, explorer-jes, explorer-uss
   # - app-server, zss
-  validate_node_home 2>&1 | print_formatted_info "${LOGGING_SERVICE_ID}" "${LOGGING_SCRIPT_NAME}:${LINENO}" -
+  validate_node_home 2>&1 | print_formatted_info "ZWELS" "prepare-instance.sh,global_validate:${LINENO}" -
 
   # validate java for some core components
   if [[ ${LAUNCH_COMPONENTS} == *"gateway"* || ${LAUNCH_COMPONENTS} == *"discovery"* || ${LAUNCH_COMPONENTS} == *"api-catalog"* || ${LAUNCH_COMPONENTS} == *"caching-service"* || ${LAUNCH_COMPONENTS} == *"files-api"* || ${LAUNCH_COMPONENTS} == *"jobs-api"* ]]; then
-    validate_java_home 2>&1 | print_formatted_info "${LOGGING_SERVICE_ID}" "${LOGGING_SCRIPT_NAME}:${LINENO}" -
+    validate_java_home 2>&1 | print_formatted_info "ZWELS" "prepare-instance.sh,global_validate:${LINENO}" -
   fi
 
   # validate z/OSMF for some core components
   if [[ ${LAUNCH_COMPONENTS} == *"discovery"* || ${LAUNCH_COMPONENTS} == *"files-api"* || ${LAUNCH_COMPONENTS} == *"jobs-api"* ]]; then
-    validate_zosmf_host_and_port "${ZOSMF_HOST}" "${ZOSMF_PORT}" 2>&1 | print_formatted_info "${LOGGING_SERVICE_ID}" "${LOGGING_SCRIPT_NAME}:${LINENO}" -
+    validate_zosmf_host_and_port "${ZOSMF_HOST}" "${ZOSMF_PORT}" 2>&1 | print_formatted_info "ZWELS" "prepare-instance.sh,global_validate:${LINENO}" -
   fi
 
   # Summary errors check, exit if errors found
