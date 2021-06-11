885c22d781a0054c1339c44a6d754047b014f126
1000TurquoisePogs <sgrady@rocketsoftware.com>
2021-06-11 07:25:05 -0400

    zos to linux without yaml seems to be working
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/utils/migrate-tools.js
+++ bin/utils/migrate-tools.js
@@ -2,12 +2,13 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
-
+console.log('js type='+process.env['KEYSTORE_TYPE']);
+console.log('keystore='+process.env['KEYSTORE_DIRECTORY']);
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
@@ -35,8 +36,8 @@ if (commandArgs.length != 4) {
 
 const instanceDir = commandArgs[0];
 const outputDir = commandArgs[1];
-const toType = commandArgs[2];
-const fromType = commandArgs[3];
+const toType = types[commandArgs[2]];
+const fromType = types[commandArgs[3]];
 
 if (!process.env['KEYSTORE_TYPE']) {
   console.error(`Could not read KEYSTORE_TYPE env var. Env vars not set correctly for migration tool.`);
@@ -45,30 +46,39 @@ if (!process.env['KEYSTORE_TYPE']) {
 
 const WORKSPACE_DIR = path.join(instanceDir, 'workspace');
 const KEYSTORE_ENV_FILE = path.join(process.env['KEYSTORE_DIRECTORY'], 'zowe-certificates.env');
+const INSTANCE_PATHS = {
+  'ROOT_DIR': "/home/zowe/install",
+  'JAVA_HOME': "/usr/local/openjdk-8",
+  'NODE_HOME':'/usr/local/node',
+  'KEYSTORE_DIRECTORY': "/home/zowe/certs"
+}
 
+const KEYSTORE_KEYS = ['KEYSTORE', 'TRUSTSTORE', 'KEYSTORE_KEY', 'KEYSTORE_CERTIFICATE', 'KEYSTORE_CERTIFICATE_AUTHORITY'];
+
+const CONVERT_DIR_EXCEPTIONS = [ 'workspace/api-mediation/api-defs' ];
 
 if (toType == fromType) {
   //this may be used to copy-paste duplicate instances in the future?
   console.error(`Error: same type migration not yet implemented`);
   process.exit(2);
 }
-
-if (toType == TYPE_ZOS && fromType == TYPE_CONTAINER) {
+if (fromType == TYPE_ZOS && toType == TYPE_CONTAINER) {
   migrateZosContainer();
-} else if (toType == TYPE_ZOS && fromType == TYPE_CONTAINER_BUNDLE) {
+} else if (fromType == TYPE_ZOS && toType == TYPE_CONTAINER_BUNDLE) {
   migrateZosBundle();
-} else if (toType == TYPE_CONTAINER && fromType == TYPE_ZOS) {
+} else if (fromType == TYPE_CONTAINER && toType == TYPE_ZOS) {
   migrateContainerZos();
-} else if (toType == TYPE_CONTAINER_BUNDLE && fromType == TYPE_ZOS) {
+} else if (fromType == TYPE_CONTAINER_BUNDLE && toType == TYPE_ZOS) {
   migrateBundleZos();
-} else if (toType == TYPE_CONTAINER_BUNDLE && fromType == TYPE_CONTAINER) {
+} else if (fromType == TYPE_CONTAINER_BUNDLE && toType == TYPE_CONTAINER) {
   migrateBundleContainer();
-} else if (toType == TYPE_CONTAINER && fromType == TYPE_CONTAINER_BUNDLE) {
+} else if (fromType == TYPE_CONTAINER && toType == TYPE_CONTAINER_BUNDLE) {
   migrateContainerBundle();
 } else {
   console.error(`Error: unimplemented migration from ${fromType} to ${toType}`);
   process.exit(3);
 }
+console.warn("Path references for 3rd party components are not altered by this tool. Please review third party component documentation if further correction is needed");
 
 function migrateZosContainer() {
   console.error('nyi migrateZosContainer');
@@ -92,8 +102,9 @@ function migrateZosBundle() {
   migrateKeystoreZosBundle();
 
   if (isZos) {
+    migrateConfigZosBundle();
     //simple conversion: reading files and writing them will do ebcdic-ascii conversion automatically!
-    convertRecursively(path.join(instanceDir, 'workspace'), path.join(outputDir, 'instance', 'workspace', undefined));
+    convertRecursively(path.join(instanceDir, 'workspace'), path.join(outputDir, 'instance', 'workspace'), undefined, WORKSPACE_DIR, simpleConvert);
   } else {
     //no tagging info, this is going to be guesswork.
     console.error(`Error: Cannot migrate z/OS instance while not on z/OS. Rerun this script on z/OS.`);
@@ -129,8 +140,9 @@ function migrateKeystoreZosBundle() {
     console.error(`Keystore type ${process.env['KEYSTORE_TYPE']} is not supported for migration`);
     process.exit(1);
   }
-  fs.readFileSync(KEYSTORE_ENV_FILE,
-  convertRecursively(process.env['KEYSTORE_DIRECTORY'], path.join(outputDir, 'instance', ), undefined, WORKSPACE_DIR, certConvert);
+  fs.writeFileSync(path.join(outputDir, 'keystore', 'zowe-certificates.env'), fs.readFileSync(KEYSTORE_ENV_FILE,'utf8'));
+  convertRecursively(process.env['KEYSTORE_DIRECTORY'], path.join(outputDir, 'keystore'), undefined, WORKSPACE_DIR, certConvert);
+  setDockerKeystorePaths();
 }
 
 function migrateConfigZosBundle() {
@@ -139,27 +151,70 @@ function migrateConfigZosBundle() {
   setDockerInstancePaths();
 }
 
-const INSTANCE_PATHS = {
-  'ROOT_DIR': "/home/zowe/install",
-  'JAVA_HOME': "/usr/local/openjdk-8",
-  'NODE_HOME':'/usr/local/node',
-  'KEYSTORE_DIRECTORY': "/home/zowe/certs"
+function setDockerKeystorePaths() {
+  let keystore = fs.readFileSync(path.join(outputDir, 'keystore', 'zowe-certificates.env'), 'utf8');
+  let lines = keystore.split('\n');
+  let keystoreDir = process.env['KEYSTORE_DIRECTORY'];
+  const migrateDir = path.join(outputDir, 'keystore', 'migrated');
+
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i].trim();
+    for (let j = 0; j < KEYSTORE_KEYS.length; j++) {
+      let key = KEYSTORE_KEYS[j];
+      if (line.startsWith(key+'=')) {
+        value=line.substring(key.length+1);
+        if (value.startsWith(keystoreDir)) {
+          lines[i] = key + '=' + value.replace(keystoreDir, INSTANCE_PATHS.KEYSTORE_DIRECTORY);
+        } else {
+          //object does not originate in keystore. copy it out
+          let filename = path.basename(value);
+          let buf = fs.readFileSync(value);
+          try {
+            fs.mkdirSync(migrateDir);
+          } catch (e) {
+            if (e.code != 'EEXIST') {
+              throw e;
+            }
+          }
+
+          let destinationPath = path.join(migrateDir, filename);
+          if (isTextCert(buf)) {
+            fs.writeFileSync(destinationPath, fs.readFileSync(value, 'utf8'));
+          } else {
+            fs.writeFileSync(destinationPath, buf);
+          }
+          lines[i] = key + '=' + path.join(INSTANCE_PATHS.KEYSTORE_DIRECTORY, 'migrated', filename);
+        }
+        break;
+      }
+    };
+  }
+  keystore = lines.join('\n');
+  fs.writeFileSync(path.join(outputDir, 'keystore', 'zowe-certificates.env'), keystore);
 }
+
 
 function setDockerInstancePaths() {
   let instance = fs.readFileSync(path.join(outputDir, 'instance', 'instance.env'), 'utf8');
   let lines = instance.split('\n');
-  const paths = Object.keys(INSTANCE_PATHS);
+  const keys = Object.keys(INSTANCE_PATHS);
   for (let i = 0; i < lines.length; i++) {
     let line = lines[i].trim();
-    paths.forEach((path)=> {
-      if (line.startsWith(path+'=')) {
-        line = path+'='+INSTANCE_PATHS[path];
+    for (let j = 0; j < keys.length; j++) {
+      let key = keys[j];
+      if (line.startsWith(key+'=')) {
+        lines[i] = key+'='+INSTANCE_PATHS[key];
       }
-    });
+    };
   }
   instance = lines.join('\n');
-  fs.writeFileSync(path.join(outputDir, 'instance', 'instance.env'));
+  fs.writeFileSync(path.join(outputDir, 'instance', 'instance.env'), instance);
+}
+
+function isTextCert(buff) {
+  const char = buff[0];
+  //look for -----, 0x60, 0x2d, 0x05
+  return buff.length > 5 && (buff[0] == buff[1]) && (buff[0] == buff[2]) && (buff[0] == buff[3]) && (buff[0] == buff[4]) && (char == 0x05 || char == 0x2d);
 }
 
 
@@ -169,9 +224,7 @@ function certConvert(fullPath, destinationPath, convertMap) {
     //copy
     fs.writeFileSync(destinationPath, buff);
   } else {
-    const char = buff[0];
-    //look for -----
-    if (buff.length > 5 && (buff[0] == buff[1] == buff[2] == buff[3] == buff[4]) && (char == 0x60 || char == 0x2d)) {
+    if (isTextCert(buff)) {
       //likely to be a text file cert or key
       if (!convertMap) {
         fs.writeFileSync(destinationPath, fs.readFileSync(fullPath, 'utf8'));
@@ -183,10 +236,10 @@ function certConvert(fullPath, destinationPath, convertMap) {
         fs.writeFileSync(destinationPath, content);
       }
     } else { //i guess its binary
-      console.warn(`file '${fullPath}' not identified as text. first char=${char}`);
+      console.warn(`file '${fullPath}' not identified as text. first char=${buff[0]}`);
       fs.writeFileSync(destinationPath, buff);
     }
-    
+
   }
 }
 
@@ -194,19 +247,35 @@ function simpleConvert(fullPath, destinationPath, convertMap) {
   if (!convertMap) {
     //TODO make more efficient by streaming buffer
     fs.writeFileSync(destinationPath, fs.readFileSync(fullPath, 'utf8'));
-  } else {
+  } else if (convertMap != 'copy') {
     const content = fs.readFileSync(fullPath);
     for (let i = 0; i < content.length; i++) {
       content[i] = convertMap[i];
     }
     fs.writeFileSync(destinationPath, content);
+  } else {
+    fs.copyFileSync(fullPath, destinationPath);
   }
 }
+
 
 //if convertMap is null, we use nodejs string conversion trick of zos to input ??? and output ascii
 //TODO make async
 function convertRecursively(inputPath, outputPath, convertMap, ignorePath, convertFunction) {
-  fs.mkdirSync(outputPath);
+  try {
+    fs.mkdirSync(outputPath);
+  } catch (e) {
+    if (e.code != 'EEXIST') {
+      throw e;
+    }
+  }
+  let dontConvert = false;
+  for (let i = 0; i < CONVERT_DIR_EXCEPTIONS.length; i++) {
+    if (inputPath.endsWith(CONVERT_DIR_EXCEPTIONS[i])) {
+      dontConvert = true;
+    }
+  }
+
   const names = fs.readdirSync(inputPath);
   names.forEach(function(name) {
     const fullPath = path.join(inputPath, name);
@@ -214,10 +283,16 @@ function convertRecursively(inputPath, outputPath, convertMap, ignorePath, conve
     if (fullPath != ignorePath) {
       const stat = fs.statSync(fullPath);
       if (stat.isDirectory()) {
-        fs.mkdirSync(destinationPath);
-        convertRecursively(fullPath, destinationPath, convertMap, ignorePath);
+        try {
+          fs.mkdirSync(destinationPath);
+        } catch (e) {
+          if (e.code != 'EEXIST') {
+            throw e;
+          }
+        }
+        convertRecursively(fullPath, destinationPath, convertMap, ignorePath, convertFunction);
       } else {
-        convertFunction(fullPath, destinationPath, convertMap);
+        convertFunction(fullPath, destinationPath, dontConvert ? 'copy' : convertMap);
       }
     }
   });
--- bin/zowe-migrate-instance.sh
+++ bin/zowe-migrate-instance.sh
@@ -7,61 +7,116 @@
 #
 # Copyright Contributors to the Zowe Project.
 
-if [ $# -lt 4 ]; then
-  echo "Usage: $0 -i instance_directory -o output_directory -t to_install_type -f from_install_type\n Types: zos, container, docker, kubernetes, docker-bundle"
+if [ $# -lt 8 ]; then
+  echo "Usage: $0 -i instance_directory -o output_directory -t to_install_type -f from_install_type [-k input_keystore_directory] [-d output_keystore_directory] [-r output_root_directory]\n Types: zos, container, docker, kubernetes, docker-bundle"
   exit 1
 fi
 
 # set defaults here
 
 
-while getopts "i:t:f" opt; do
-  case $opt in
-    i) INSTANCE_DIR=$OPTARG;;
-    k) KEYSTORE_DIR=$OPTARG;;
-    t) TO_TYPE=$OPTARG;;
-    f) FROM_TYPE=$OPTARG;;
-    o) OUTPUT_DIR=$OPTARG;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
+while [ $# -gt 0 ]; do
+  arg="$1"
+  case $arg in
+    -i)
+        shift
+        INSTANCE_DIR=$1
+        shift
+    ;;
+    -k)
+        shift
+        KEYSTORE_DIR=$1
+        shift
+    ;;
+    -t)
+        shift
+        TO_TYPE=$1
+        shift
+    ;;
+    -f)
+        shift
+        FROM_TYPE=$1
+        shift
+    ;;
+    -o)
+        shift
+        OUTPUT_DIR=$1
+        shift
+    ;;
+    -r)
+        shift
+        export MIGRATE_ROOT_DIR=$1
+        shift
+    ;;
+    -d)
+        shift
+        export MIGRATE_KEYSTORE_DIRECTORY=$1
+        shift
+    ;;
+    *)
+      echo "Invalid option: $1" >&2
       exit 1
-      ;;
   esac
 done
-shift $(($OPTIND-1))
 
-if [ -e "$INSTANCE_DIR/instance.env" ]; then
-  mkdir -p $OUTPUT_DIR
-
-  readInstance()
-  if [ -z "$KEYSTORE_DIR" ]; then
-    KEYSTORE_DIR="$KEYSTORE_DIRECTORY"
-  fi
-  readKeystore()
-  cd utils
-  node migrate-tools.js "$INSTANCE_DIR" "$OUTPUT_DIR" "$TO_TYPE" "$FROM_TYPE"
-  else
-    echo "Error: cannot read instance.env"
-  fi
-else
-  echo "Error: cannot read INSTANCE_DIR file read-essential-vars.sh"
+if [ -z $INSTANCE_DIR -o -z $TO_TYPE -o -z $FROM_TYPE -o -z OUTPUT_DIR ]; then
+  echo "Usage: $0 -i instance_directory -o output_directory -t to_install_type -f from_install_type [-k keystore_directory]\n Types: zos, container, docker, kubernetes, docker-bundle"
   exit 1
 fi
 
 readInstance() {
+  mkdir -p $OUTPUT_DIR/instance
+
   . "$INSTANCE_DIR/instance.env"
   if [ $? -ne 0 ]; then
-    mkdir -p $OUTPUT_DIR/instance
+    echo "cant read instance"
     iconv -f 819 -t 1047 "$INSTANCE_DIR/instance.env" > "$OUTPUT_DIR/instance/instance.env.iconv1047"
-    . "$OUTPUT_DIR/instance/instance.env.iconv1047"
+    source_env "$OUTPUT_DIR/instance/instance.env.iconv1047"
+  else
+    source_env "$INSTANCE_DIR/instance.env"
   fi
 }
 
 readKeystore() {
+  mkdir -p $OUTPUT_DIR/keystore
+
   . "$KEYSTORE_DIR/zowe-certificates.env"
   if [ $? -ne 0 ]; then
-    mkdir -p $OUTPUT_DIR/keystore
+    echo "cant read keystore"
     iconv -f 819 -t 1047 "$KEYSTORE_DIR/zowe-certificates.env" > "$OUTPUT_DIR/keystore/zowe-certificates.env.iconv1047"
-    . "$OUTPUT_DIR/keystore/zowe-certificates.env.iconv1047"
-  fi    
+    source_env "$OUTPUT_DIR/keystore/zowe-certificates.env.iconv1047"
+  else
+    source_env "$KEYSTORE_DIR/zowe-certificates.env"
+  fi
 }
+
+source_env() {
+  env_file=$1
+
+  . "${env_file}"
+
+  while read -r line ; do
+    # skip line if first char is #
+    test -z "${line%%#*}" && continue
+    key=${line%%=*}
+    export $key
+  done < "${env_file}"
+}
+
+if [ -e "$INSTANCE_DIR/instance.env" ]; then
+  echo "mkdir -p $OUTPUT_DIR"
+  mkdir -p $OUTPUT_DIR
+
+  readInstance
+  echo "keystore dir=$KEYSTORE_DIRECTORY"
+  if [ -z "$KEYSTORE_DIR" ]; then
+    KEYSTORE_DIR="$KEYSTORE_DIRECTORY"
+  fi
+  readKeystore
+
+  cd utils
+  node migrate-tools.js "$INSTANCE_DIR" "$OUTPUT_DIR" "$TO_TYPE" "$FROM_TYPE"
+else
+  echo "Error: cannot read instance.env"
+  exit 1
+fi
