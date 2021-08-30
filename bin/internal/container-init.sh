16cf54694d1d10b500a4a381e153917eda408fba
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-08-30 16:28:51 -0400

    test discovery/api-catalog
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

---
+++ bin/internal/container-init.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#######################################################################
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v2.0 which
+# accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-v20.html
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Copyright Contributors to the Zowe Project.
+#######################################################################
+
+#######################################################################
+# Prepare runtime directory when Zowe is running in containers
+
+# exit if there are errors
+set -e
+
+#######################################################################
+# Constants
+SOURCE_DIR=/component
+RUNTIME_DIR=/home/zowe/runtime
+INSTANCE_DIR=/home/zowe/instance
+WORKSPACE_DIR=${INSTANCE_DIR}/workspace
+STATIC_DEF_CONFIG_DIR=${WORKSPACE_DIR}/api-mediation/api-defs
+POD_NAME=$(hostname -s 2>/dev/null)
+
+#######################################################################
+echo ">>> delete static definitions written by current pod ${POD_NAME}"
+if [ -d "${STATIC_DEF_CONFIG_DIR}" -a -n "${POD_NAME}" ]; then
+  cd "${STATIC_DEF_CONFIG_DIR}"
+  ls *.${POD_NAME}.yaml
+  rm -f *.${POD_NAME}.yaml
+fi
+
+#######################################################################
+echo ">>> done"
