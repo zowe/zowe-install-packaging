c8d6386d0349670565c85411ebcc60dd25d6a088
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-09-10 08:50:41 -0400

    must use default component ports
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/instance/utils/convert-for-k8s.sh
+++ bin/instance/utils/convert-for-k8s.sh
@@ -100,6 +100,16 @@ NEW_INSATNCE_ENV_CONTENT=$(cat "${INSTANCE_DIR}"/instance.env | \
   grep -v -E "ZOWE_EXPLORER_HOST=" | \
   sed -e "s#ROOT_DIR=.\+\$#ROOT_DIR=/home/zowe/runtime#" | \
   sed -e "s#KEYSTORE_DIRECTORY=.\+\$#KEYSTORE_DIRECTORY=/home/zowe/keystore#" | \
+  sed -e "s#CATALOG_PORT=.\+\$#CATALOG_PORT=7552#" | \
+  sed -e "s#DISCOVERY_PORT=.\+\$#DISCOVERY_PORT=7553#" | \
+  sed -e "s#GATEWAY_PORT=.\+\$#GATEWAY_PORT=7554#" | \
+  sed -e "s#ZWE_CACHING_SERVICE_PORT=.\+\$#ZWE_CACHING_SERVICE_PORT=7555#" | \
+  sed -e "s#JOBS_API_PORT=.\+\$#JOBS_API_PORT=8545#" | \
+  sed -e "s#FILES_API_PORT=.\+\$#FILES_API_PORT=8547#" | \
+  sed -e "s#JES_EXPLORER_UI_PORT=.\+\$#JES_EXPLORER_UI_PORT=8546#" | \
+  sed -e "s#MVS_EXPLORER_UI_PORT=.\+\$#MVS_EXPLORER_UI_PORT=8548#" | \
+  sed -e "s#USS_EXPLORER_UI_PORT=.\+\$#USS_EXPLORER_UI_PORT=8550#" | \
+  sed -e "s#ZOWE_ZLUX_SERVER_HTTPS_PORT=.\+\$#ZOWE_ZLUX_SERVER_HTTPS_PORT=8544#" | \
   sed -e "s#ZWE_DISCOVERY_SERVICES_LIST=.\+\$#ZWE_DISCOVERY_SERVICES_REPLICAS=1#" | \
   sed -e "s#APIML_GATEWAY_EXTERNAL_MAPPER=.\+\$#APIML_GATEWAY_EXTERNAL_MAPPER=https://\${GATEWAY_HOST}:\${GATEWAY_PORT}/zss/api/v1/certificate/x509/map#" | \
   sed -e "s#APIML_SECURITY_AUTHORIZATION_ENDPOINT_URL=.\+\$#APIML_SECURITY_AUTHORIZATION_ENDPOINT_URL=https://\${GATEWAY_HOST}:\${GATEWAY_PORT}/zss/api/v1/saf-auth#" | \
