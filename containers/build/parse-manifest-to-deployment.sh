a0aee165ce32d73d054acc83b78547b368edb79d
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-10-01 15:06:19 -0400

    script update to replace version from manifest
    Signed-off-by: Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>

--- containers/build/parse-manifest-to-deployment.sh
+++ containers/build/parse-manifest-to-deployment.sh
@@ -7,13 +7,16 @@
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_PWD=$(cd "$(dirname "$0")" && pwd)
 PROJECT_ROOT_PWD=$(cd "$SCRIPT_PWD" && cd ../.. && pwd)
-manifestFile="$PROJECT_ROOT_PWD/manifest.json"
+K8S_ROOT_PWD=$(cd "$PROJECT_ROOT_PWD" && cd containers/kubernetes && pwd)
+MANIFEST_FILE="$PROJECT_ROOT_PWD/manifest.json"
 
-for eachImageDep in $(jq -r '.imageDependencies | to_entries[] | .key' $manifestFile);
+# parse manifest to look for imageDependencies
+for eachImageDep in $(jq -r '.imageDependencies | to_entries[] | .key' $MANIFEST_FILE);
 do
-    registry=$(jq -r ".imageDependencies.\"$eachImageDep\" | .registry" $manifestFile)
-    name=$(jq -r ".imageDependencies.\"$eachImageDep\" | .name" $manifestFile)
-    tag=$(jq -r ".imageDependencies.\"$eachImageDep\" | .tag" $manifestFile)
+    registry=$(jq -r ".imageDependencies.\"$eachImageDep\" | .registry" $MANIFEST_FILE)
+    name=$(jq -r ".imageDependencies.\"$eachImageDep\" | .name" $MANIFEST_FILE)
+    tag=$(jq -r ".imageDependencies.\"$eachImageDep\" | .tag" $MANIFEST_FILE)
+    # construct image line
     image="$registry/$name:$tag"
 
     if [[ -n "$debug" ]]; 
@@ -32,7 +35,8 @@ do
     else
         yamlFile="$PROJECT_ROOT_PWD/containers/kubernetes/workloads/$eachImageDep-deployment.yaml"
     fi    
-    
+
+    # replace image line with information parsed from manifest
     if [ "$eachImageDep" = "cleanup-static-definitions-cronjob" ];
     then
         IMAGE=$image yq e -i '.spec.jobTemplate.spec.template.spec.containers[0].image = strenv(IMAGE)' $yamlFile 
@@ -41,3 +45,23 @@ do
     fi
 done
 
+# parse version in manifest and replace each file in kubernetes directory with current version
+# ie. app.kubernetes.io/version: <CURRENT_PROJECT_VERSION>
+CURRENT_PROJECT_VERSION=$(jq -r '.version' $MANIFEST_FILE)
+i=0 #counter, for debug purpose
+filesArray= #store an array of matched files, for debug purpose
+for eachMatchedFile in $(grep -rlw $K8S_ROOT_PWD -e 'app.kubernetes.io/version:' --exclude 'sample-deployment.yaml');
+do
+    VERSION=$CURRENT_PROJECT_VERSION yq e -i '.metadata.labels."app.kubernetes.io/version" = strenv(VERSION)' $eachMatchedFile 
+    let i++
+    eachMatchedFile=${eachMatchedFile##*kubernetes/}
+    filesArray="${filesArray}${eachMatchedFile}"$'\n'
+done
+
+if [[ -n "$debug" ]];
+then
+    echo "There are $i files changed with line app.kubernetes.io/version: $CURRENT_PROJECT_VERSION, they are: "
+    echo "$filesArray"
+fi
+
+echo "SUCCESS! Yaml files replacement from manifest parsing complete!"
