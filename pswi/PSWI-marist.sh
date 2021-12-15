8fa40829d7c5cd540b5db5f49d43bdeba12ddc5c
mm667937 <marketa.muzikova@broadcom.com>
2021-12-15 22:37:35 +0100

    deleted bin command
    Signed-off-by: mm667937 <marketa.muzikova@broadcom.com>

--- pswi/PSWI-marist.sh
+++ pswi/PSWI-marist.sh
@@ -111,7 +111,6 @@ HOST=${ZOSMF_URL#https:\/\/}
 
 sshpass -p${ZOSMF_PASS} sftp -o BatchMode=no -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -b - -P 22 ${ZOSMF_USER}@${HOST} << EOF
 cd ${DIR}
-bin
 put ${RFDSNPFX}.${FMID}.${PTF1} ${PTF1}
 put ${RFDSNPFX}.${FMID}.${PTF2} ${PTF2}
 cp ${PTF1} "//'${SMPMCS}.${RFDSNPFX}.${FMID}.${PTF1}'"
