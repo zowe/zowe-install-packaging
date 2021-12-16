fe288b81172c608bc5659e31592c4fead2a179d2
at670475 <andrea.tabone@broadcom.com>
2021-12-16 11:37:26 +0100

    Remove variable
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- tests/sanity/test/apiml/test-api-gateway-sample-extension-controller.js
+++ tests/sanity/test/apiml/test-api-gateway-sample-extension-controller.js
@@ -18,30 +18,28 @@ let REQ, username, password;
 
 
 describe('test api gateway sample extension controller', function() {
-  if (process.env.ZWE_GATEWAY_SHARED_LIBS) {
-    before('verify environment variables', function() {
-      // allow self signed certs
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  before('verify environment variables', function() {
+    // allow self signed certs
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-      expect(process.env.ZOWE_EXTERNAL_HOST, 'ZOWE_EXTERNAL_HOST is empty').to.not.be.empty;
-      expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
-      expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
-      expect(process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT, 'ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT is not defined').to.not.be.empty;
+    expect(process.env.ZOWE_EXTERNAL_HOST, 'ZOWE_EXTERNAL_HOST is empty').to.not.be.empty;
+    expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
+    expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
+    expect(process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT, 'ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT is not defined').to.not.be.empty;
 
-      REQ = axios.create({
-        baseURL: `https://${process.env.ZOWE_EXTERNAL_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`,
-        timeout: 30000,
-      });
-      username = process.env.SSH_USER;
-      password = process.env.SSH_PASSWD;
-      debug(`Explorer server URL: https://${process.env.ZOWE_EXTERNAL_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`);
+    REQ = axios.create({
+      baseURL: `https://${process.env.ZOWE_EXTERNAL_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`,
+      timeout: 30000,
     });
+    username = process.env.SSH_USER;
+    password = process.env.SSH_PASSWD;
+    debug(`Explorer server URL: https://${process.env.ZOWE_EXTERNAL_HOST}:${process.env.ZOWE_API_MEDIATION_GATEWAY_HTTP_PORT}`);
+  });
 
-    it('should return the greeting message from the gateway sample extension controller', function() {
-      debug('Verify access to greeting endpoint via /api/v1/greeting');
-      return getAndVerifyGreeting('/api/v1/greeting');
-    });
-  }
+  it('should return the greeting message from the gateway sample extension controller', function() {
+    debug('Verify access to greeting endpoint via /api/v1/greeting');
+    return getAndVerifyGreeting('/api/v1/greeting');
+  });
 
   function getAndVerifyGreeting(url) {
     const _this = this;
