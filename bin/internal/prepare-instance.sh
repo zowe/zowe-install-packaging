f9a2748e6623e8fa38cb46ae44642be1676186aa
at670475 <andrea.tabone@broadcom.com>
2021-12-20 16:13:32 +0100

    Even more debug 4
    Signed-off-by: at670475 <andrea.tabone@broadcom.com>

--- bin/internal/prepare-instance.sh
+++ bin/internal/prepare-instance.sh
@@ -326,8 +326,7 @@ configure_components() {
       echo "${retval}"
       print_formatted_debug "ZWELS" "here i am" "sharedLibs"
       ls -la "${WORKSPACE_DIR}"/gateway/sharedLibs/
-      ls -la "${WORKSPACE_DIR}"/gateway/sharedLibs/apiml-sample-extension/
-      ls -la "${WORKSPACE_DIR}"/gateway/sharedLibs/org.zowe.apiml.sdk.apiml-sample-extension-package/
+      ls -la "${WORKSPACE_DIR}"/gateway/sharedLibs/org.zowe.apiml.sdk.apiml-sample-extension-package/bin/
       if [ -n "${result}" ]; then
         if [ "${retval}" = "0" ]; then
           print_formatted_debug "ZWELS" "prepare-instance.sh,configure_components:${LINENO}" "${result}"
