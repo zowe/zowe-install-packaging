0a436359a9aa53a1ee73af9cbe3559682a802077
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2022-01-18 13:42:18 -0500

    command config get|set and env get
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/commands/internal/env/get/index.sh
+++ bin/commands/internal/env/get/index.sh
@@ -15,5 +15,15 @@
 # validation
 require_zowe_yaml
 
+# check and sanitize ZWE_CLI_PARAMETER_HA_INSTANCE
+sanitize_ha_instance_id
+
+ZWE_zowe_workspaceDirectory=$(read_yaml ${ZWE_CLI_PARAMETER_CONFIG} '.zowe.workspaceDirectory')
+if [ -z "${ZWE_zowe_workspaceDirectory}" ]; then
+  print_error_and_exit "Error ZWEL0157E: Zowe workspace directory (zowe.workspaceDirectory) is not defined in Zowe YAML configuration file." "" 157
+fi
+
 ###############################
-read_yaml "${ZWE_CLI_PARAMETER_CONFIG}" ".${ZWE_CLI_PARAMETER_CONFIGURATION_PATH}"
+load_environment_variables
+
+get_var_value "${ZWE_CLI_PARAMETER_VARIABLE}"
