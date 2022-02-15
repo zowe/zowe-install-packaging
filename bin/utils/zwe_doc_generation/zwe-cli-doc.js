df87a119f2c6359b7737b7f6ac87660e7c65814b
Carson Cook <carson.cook@ibm.com>
2022-02-15 14:12:39 -0500

    Propagate parent group in command docs
    Signed-off-by: Carson Cook <carson.cook@ibm.com>

--- bin/utils/zwe_doc_generation/zwe-cli-doc.js
+++ bin/utils/zwe_doc_generation/zwe-cli-doc.js
@@ -14,31 +14,41 @@ const path = require('path');
 const docsRootDirectory = path.join(__dirname, '../../commands');
 const generatedDocDirectory = path.join(__dirname, './generated');
 
+const HELP = {
+    fileName: '.help',
+    key: 'help',
+    order: 1,
+    title: 'Help'
+};
+const EXAMPLES = {
+    fileName: '.examples',
+    key: 'examples',
+    order: 2,
+    title: 'Examples'
+};
+const PARAMETERS = {
+    fileName: '.parameters',
+    key: 'parameters',
+    order: 3,
+    title: 'Parameters'
+};
+const EXCLUSIVE_PARAMETERS = {
+    fileName: '.exclusive-parameters',
+    key: 'exclusive-parameters'
+};
+const ERRORS = {
+    fileName: '.errors',
+    key: 'errors',
+    order: 4,
+    title: 'Errors'
+};
+const EXPERIMENTAL = {
+    fileName: '.experimental',
+    key: 'experimental'
+};
+
 const documentationFiles = [
-    {
-        fileName: '.help',
-        use: 'help'
-    },
-    {
-        fileName: '.examples',
-        use: 'examples'
-    },
-    {
-        fileName: '.parameters',
-        use: 'parameters'
-    },
-    {
-        fileName: '.exclusive-parameters',
-        use: 'exclusive-parameters'
-    },
-    {
-        fileName: '.errors',
-        use: 'errors'
-    },
-    {
-        fileName: '.experimental',
-        use: 'experimental'
-    }
+    HELP, EXAMPLES, PARAMETERS, EXCLUSIVE_PARAMETERS, ERRORS, EXPERIMENTAL
 ]
 
 const docsTree = getDocumentationTree(docsRootDirectory);
@@ -58,7 +68,10 @@ function getDocumentationTree(directory) {
             const docFileType = documentationFiles.find((df) => df.fileName === file);
             if (docFileType) {
                 documentationNode.command = path.basename(directory);
-                documentationNode[docFileType.use] = objectPath;
+                if (documentationNode.command === 'commands') {
+                    documentationNode.command = 'zwe'; // special case for root directory name
+                }
+                documentationNode[docFileType.key] = objectPath;
             }
         }
     }
@@ -66,26 +79,32 @@ function getDocumentationTree(directory) {
     return documentationNode;
 }
 
-function writeMdFiles(docsNode) {
+function writeMdFiles(docsNode, parent) {
+    const command = parent ? `${parent} ${docsNode.command}` : docsNode.command;
     if (docsNode.children && docsNode.children.length) {
         for (const child of docsNode.children) {
-            writeMdFiles(child);
+            writeMdFiles(child, command);
         }
     }
 
-    // TODO need to apply parent parameters to child docs
-    const mdContent = `# zwe ${docsNode.command}${getMdContentForNode(docsNode)}`;
-    fs.writeFileSync(`${generatedDocDirectory}/zwe-doc-${docsNode.command}.md`, mdContent);
+    const mdContent = getMdContentForNode(docsNode, command);
+    fs.writeFileSync(`${generatedDocDirectory}/doc-${command.replace(/\s/g, '-')}.md`, mdContent);
 }
 
-function getMdContentForNode(docNode) {
+function getMdContentForNode(docNode, command) {
     // TODO need to order the file content
     // TODO need to transform from . file format to md format (e.g. tables)
-    let mdContent = "";
+    // TODO need to apply parent experimental, parameters and errors to child docs
+    let mdContent = `# ${command}`;
+
+    if (docNode[EXPERIMENTAL.key]) {
+        mdContent = mdContent + '\n\n**Warning:** This command is for experimental purposes and may not fully function.';
+    }
+
     for (const fileType of documentationFiles) {
-        if (docNode[fileType.use]) {
-            const fileContent = fs.readFileSync(docNode[fileType.use], 'utf-8');
-            mdContent = mdContent.concat(`\n\n## ${fileType.use}\n\n${fileContent}`);
+        if (docNode[fileType.key]) {
+            const fileContent = fs.readFileSync(docNode[fileType.key], 'utf-8');
+            mdContent = mdContent + `\n\n## ${fileType.key}\n\n${fileContent}`;
         }
     }
 
