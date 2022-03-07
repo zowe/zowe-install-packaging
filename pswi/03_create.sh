0f6efd3ec4b53ee801f074cfaadfc12570fb6d9c
mm667937 <marketa.muzikova@broadcom.com>
2022-03-07 19:10:42 +0100

    zOSMF can't open UTF8 workflows from a dataset
    Signed-off-by: mm667937 <marketa.muzikova@broadcom.com>

--- pswi/03_create.sh
+++ pswi/03_create.sh
@@ -127,21 +127,27 @@ echo "//STDOUT DD SYSOUT=*" >> JCL
 echo "//STDERR DD SYSOUT=*" >> JCL
 echo "//STDPARM  DD *" >> JCL
 echo "SH set -x;set -e;" >> JCL
+echo "cd ${WORK_MOUNT}"
 echo "source=\"${ZOWE_MOUNT}files/workflows/ZWEWRF02.xml\";" >> JCL
 echo "target=\"//'${WORKFLOW_DSN}(ZWEWRF02)'\";" >> JCL
-echo "cp -T \$source \$target;" >> JCL
+echo "sed 's|UTF-8|IBM-1047|g' \$source > _ZWEWRF02;" >> JCL                         
+echo "cp -T _ZWEWRF02 \$target;" >> JCL
 echo "source=\"${ZOWE_MOUNT}files/workflows/ZWECRECR.xml\";" >> JCL
 echo "target=\"//'${WORKFLOW_DSN}(ZWECRECR)'\";" >> JCL
-echo "cp -T \$source \$target;" >> JCL
+echo "sed 's|UTF-8|IBM-1047|g' \$source > _ZWECRECR;" >> JCL                         
+echo "cp -T _ZWECRECR \$target;" >> JCL
 echo "source=\"${ZOWE_MOUNT}files/workflows/ZWEKRING.xml\";" >> JCL
 echo "target=\"//'${WORKFLOW_DSN}(ZWEKRING)'\";" >> JCL
-echo "cp -T \$source \$target;" >> JCL
+echo "sed 's|UTF-8|IBM-1047|g' \$source > _ZWEKRING;" >> JCL                         
+echo "cp -T _ZWEKRING \$target;" >> JCL
 echo "source=\"${ZOWE_MOUNT}files/workflows/ZWELOADC.xml\";" >> JCL
 echo "target=\"//'${WORKFLOW_DSN}(ZWELOADC)'\";" >> JCL
-echo "cp -T \$source \$target;" >> JCL
+echo "sed 's|UTF-8|IBM-1047|g' \$source > _ZWELOADC;" >> JCL                         
+echo "cp -T _ZWELOADC \$target;" >> JCL
 echo "source=\"${ZOWE_MOUNT}files/workflows/ZWESIGNC.xml\";" >> JCL
 echo "target=\"//'${WORKFLOW_DSN}(ZWESIGNC)'\";" >> JCL
-echo "cp -T \$source \$target;" >> JCL
+echo "sed 's|UTF-8|IBM-1047|g' \$source > _ZWESIGNC;" >> JCL                         
+echo "cp -T _ZWESIGNC \$target;" >> JCL
 echo "/*" >> JCL
 
 sh scripts/submit_jcl.sh "`cat JCL`"
