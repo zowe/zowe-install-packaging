8c4721bd04302978cd6690438c6a6dd0b0ee2e06
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2022-03-18 11:10:05 -0400

    better permission setup
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- bin/commands/internal/start/prepare/index.sh
+++ bin/commands/internal/start/prepare/index.sh
@@ -63,13 +63,20 @@ prepare_workspace_directory() {
   export ZWE_GATEWAY_SHARED_LIBS="${ZWE_zowe_workspaceDirectory}/gateway/sharedLibs/"
 
   mkdir -p "${ZWE_zowe_workspaceDirectory}"
-  # FIXME: 771 is inherited from v1, we should consider disable read permission for `other`
-  umask 0002
-  chmod -R 771 "${ZWE_zowe_workspaceDirectory}"
 
   if [ ! -w "${ZWE_zowe_workspaceDirectory}" ]; then
     print_formatted_error "ZWELS" "zwe-internal-start-prepare,prepare_workspace_directory:${LINENO}" "ZWEL0141E: User $(get_user_id) does not have write permission on ${ZWE_zowe_workspaceDirectory}."
     exit 141
+  fi
+
+  # set workspace dir permission
+  # FIXME: 771 is inherited from v1, we should consider disable read permission for `other`
+  umask 0002
+  result=$(chmod -R 771 "${ZWE_zowe_workspaceDirectory}" 2>&1)
+  code=$?
+  if [ ${code} -ne 0 ]; then
+    print_formatted_error "ZWELS" "zwe-internal-start-prepare,prepare_workspace_directory:${LINENO}" "WARNING: Failed to set permission of some existing files or directories in ${ZWE_zowe_workspaceDirectory}:"
+    print_formatted_error "ZWELS" "zwe-internal-start-prepare,prepare_workspace_directory:${LINENO}" "${result}"
   fi
 
   # create apiml static defs directory
