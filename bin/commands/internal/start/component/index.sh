cbd39a11af6d2ef03e89be99b7ad20a64031b9ad
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-07-07 10:18:42 -0400

    Also disable TS version of internal start component
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/commands/internal/start/component/index.sh
+++ bin/commands/internal/start/component/index.sh
@@ -11,13 +11,14 @@
 # Copyright Contributors to the Zowe Project.
 #######################################################################
 
-USE_CONFIGMGR=${ZWE_CLI_PARAMETER_CONFIGMGR}
-if [ -n "${ZWE_CLI_PARAMETER_CONFIG}" -a "${USE_CONFIGMGR}" != "true" ]; then
-  USE_CONFIGMGR=$(shell_read_yaml_config "${ZWE_CLI_PARAMETER_CONFIG}" 'zowe' 'useConfigmgr')
-fi
-if [ "${USE_CONFIGMGR}" = "true" ]; then
-  _CEE_RUNOPTS="XPLINK(ON),HEAPPOOLS(OFF)" ${ZWE_zowe_runtimeDirectory}/bin/utils/configmgr -script "${ZWE_zowe_runtimeDirectory}/bin/commands/internal/start/component/cli.js"
-else
+# TODO disabled temporarily while bugs are fixed
+#USE_CONFIGMGR=${ZWE_CLI_PARAMETER_CONFIGMGR}
+#if [ -n "${ZWE_CLI_PARAMETER_CONFIG}" -a "${USE_CONFIGMGR}" != "true" ]; then
+#  USE_CONFIGMGR=$(shell_read_yaml_config "${ZWE_CLI_PARAMETER_CONFIG}" 'zowe' 'useConfigmgr')
+#fi
+#if [ "${USE_CONFIGMGR}" = "true" ]; then
+#  _CEE_RUNOPTS="XPLINK(ON),HEAPPOOLS(OFF)" ${ZWE_zowe_runtimeDirectory}/bin/utils/configmgr -script "${ZWE_zowe_runtimeDirectory}/bin/commands/internal/start/component/cli.js"
+#else
 
 
 ###############################
@@ -89,4 +90,4 @@ else
   print_formatted_error "ZWELS" "zwe-internal-start-component:${LINENO}" "Failed to locate component directory for ${ZWE_CLI_PARAMETER_COMPONENT}."
 fi
 
-fi
+#fi
