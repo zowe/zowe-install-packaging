8aeb431eb2f7958c64dfdc60aba49219a2e95682
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-10-31 15:15:08 -0400

    Add config commands
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

---
+++ bin/commands/config/get/cli.ts
@@ -0,0 +1,17 @@
+/*
+  This program and the accompanying materials are made available
+  under the terms of the Eclipse Public License v2.0 which
+  accompanies this distribution, and is available at
+  https://www.eclipse.org/legal/epl-v20.html
+ 
+  SPDX-License-Identifier: EPL-2.0
+ 
+  Copyright Contributors to the Zowe Project.
+*/
+import * as std from 'std';
+import * as index from './../../internal/config/get/index';
+import * as configmgr from '../../../libs/configmgr';
+
+index.execute(std.getenv('ZWE_CLI_PARAMETER_PATH'), std.getenv('ZWE_CLI_PARAMETER_HA_INSTANCE'));
+
+configmgr.cleanupTempDir();
