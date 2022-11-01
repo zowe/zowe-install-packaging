2ed3a560ddee6696b6a4639f8df4b06b4923f637
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-11-01 16:12:15 -0400

    More fixes on testing
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/commands/init/security/cli.ts
+++ bin/commands/init/security/cli.ts
@@ -9,9 +9,10 @@
   Copyright Contributors to the Zowe Project.
 */
 
+import * as std from 'std';
 import * as index from './index';
 import * as configmgr from '../../../libs/configmgr';
 
-index.execute();
+index.execute(std.getenv('ZWE_CLI_PARAMETER_SECURITY_DRY_RUN') == "true", std.getenv('ZWE_CLI_PARAMETER_IGNORE_SECURITY_FAILURES') == "true");
 
 configmgr.cleanupTempDir();
