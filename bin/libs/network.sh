aaa26aa5b7e1e0a5f84fa487d7bb157f2c9397fa
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-11-30 14:30:46 -0500

    Update network.sh
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/libs/network.sh
+++ bin/libs/network.sh
@@ -102,7 +102,7 @@ is_port_available() {
 
   case $(uname) in
     "OS/390")
-      vipa_ip=${ZWE_zowe_network_vipaIp:-ZWE_zowe_environments_ZWE_NETWORK_VIPA_IP}
+      vipa_ip=${ZWE_zowe_network_vipaIp:-$ZWE_zowe_environments_ZWE_NETWORK_VIPA_IP}
       if [ -n "${vipa_ip}" ]; then
         result=$(${netstat} -B ${vipa_ip}+${port} -c SERVER 2>&1)
       else    
