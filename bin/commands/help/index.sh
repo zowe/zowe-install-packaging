4ff0749e90583f9b1b16c32a1afee801dbff8bc6
Sakshi Bobade <bobadesakshi21@gmail.com>
2023-06-16 16:59:42 +0530

    Testing

--- bin/commands/help/index.sh
+++ bin/commands/help/index.sh
@@ -11,30 +11,42 @@
 # Copyright Contributors to the Zowe Project.
 #######################################################################
 
+
+if [ "$#" -eq 0 ]; then
+    echo "No command-line arguments provided."
+else
+    echo "Command-line arguments were provided."
+fi
+
+if [ "$#" -eq 1 ]; then
+   message_id1=$1
+   echo "Message ID1: $message_id1"
+else
+  echo "Not just one parameter"
+fi
+
+if [ "$#" -eq 1 ]; then
+  argument="$1"
+  echo "Command-line argument: $argument"
+else
+  echo "No command-line argument provided or too many arguments."
+fi
+
+if [ "$#" -ge 1 ]; then
+  echo "Command-line arguments:"
+  for arg in "$@"; do
+    echo "$arg"
+  done
+else
+  echo "No command-line arguments provided."
+fi
+
 if [ "$#" -eq 2 ]; then
-  message_id=$2
-  echo "Message ID: $message_id"
+  message_id2=$2
+  echo "Message ID2: $message_id2"
 
   if [[ $message_id == ZWE* ]]; then
     echo "STARTS WITH ZWE"
-
-    fourth_letter=${message_id:3:1}
-    echo "fourth_letter: $fourth_letter"
-
-    case "$fourth_letter" in
-      "A")
-        echo "Starts with A"
-        ;;
-      "D")
-        echo "Starts with D"
-        ;;
-      "S")
-        echo "Starts with S"
-        ;;
-      *)
-        echo "Invalid fourth letter"
-        ;;
-    esac
   else
     echo "Invalid message ID"
   fi
