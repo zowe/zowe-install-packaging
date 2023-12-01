ea8e2401ac84599464faafab8f6587e46f577c87
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2023-12-01 13:41:20 +0100

    Revert "certificate keyring-jcl clean only for option 1"
    This reverts commit 8f296b593358638dc1523fe4ee20b1da5d59c0b6.

--- bin/commands/init/certificate/index.sh
+++ bin/commands/init/certificate/index.sh
@@ -349,7 +349,7 @@ if [ "${cert_type}" = "PKCS12" ]; then
 elif [[ "${cert_type}" == JCE*KS ]]; then
   # FIXME: how do we check if keyring exists without permission on RDATALIB?
   # should we clean up before creating new
-  if [ "${ZWE_CLI_PARAMETER_ALLOW_OVERWRITE}" = "true" ] && [ "${keyring_option}" = "1" ]; then
+  if [ "${ZWE_CLI_PARAMETER_ALLOW_OVERWRITE}" = "true" ]; then
     # warning
     print_message "Warning ZWEL0300W: Keyring \"safkeyring:////${keyring_owner}/${keyring_name}\" will be overwritten during configuration."
 
