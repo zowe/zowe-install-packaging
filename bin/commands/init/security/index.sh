4dcd99764f0e7c482e41fcf87b7314e076c91cd4
1000TurquoisePogs <sgrady@rocketsoftware.com>
2024-04-19 14:25:32 -0400

    Add HEAPPOOLS64(OFF)
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/commands/init/security/index.sh
+++ bin/commands/init/security/index.sh
@@ -18,7 +18,7 @@ if [ "${USE_CONFIGMGR}" = "true" ]; then
     # user-facing command, use tmpdir to not mess up workspace permissions
     export ZWE_PRIVATE_TMP_MERGED_YAML_DIR=1
   fi
-  _CEE_RUNOPTS="XPLINK(ON),HEAPPOOLS(OFF)" ${ZWE_zowe_runtimeDirectory}/bin/utils/configmgr -script "${ZWE_zowe_runtimeDirectory}/bin/commands/init/security/cli.js"
+  _CEE_RUNOPTS="XPLINK(ON),HEAPPOOLS(OFF),HEAPPOOLS64(OFF)" ${ZWE_zowe_runtimeDirectory}/bin/utils/configmgr -script "${ZWE_zowe_runtimeDirectory}/bin/commands/init/security/cli.js"
 else
   print_error_and_exit "Error ZWEL0316E: Command requires zowe.useConfigmgr=true to use." "" 316
 fi
