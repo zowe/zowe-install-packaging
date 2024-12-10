63d5d584ca87d0d6c6aa598c717f9619bf51ecb0
MarkAckert <mark.ackert@broadcom.com>
2024-12-10 15:05:30 -0500

    keep java8 for v2 pax build
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- .pax/pre-packaging.sh
+++ .pax/pre-packaging.sh
@@ -192,7 +192,7 @@ BASE_DIR=$(
 
 # use node v18 to build
 export NODE_HOME=/ZOWE/node/node-v18.16.0
-export JAVA_HOME=/ZOWE/node/J17.0_64
+export JAVA_HOME=/ZOWE/node/J8.0_64
 export PATH=$JAVA_HOME/bin:$PATH
 ZOWE_ROOT_DIR="${BASE_DIR}/content"
 
