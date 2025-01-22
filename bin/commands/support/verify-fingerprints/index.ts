eb2c970c7bfe7b8c820b3c082297ba324a20af2d
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-01-22 14:16:29 +0100

    Initial commit
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

---
+++ bin/commands/support/verify-fingerprints/index.ts
@@ -0,0 +1,159 @@
+/*
+  This program and the accompanying materials are made available
+  under the terms of the Eclipse Public License v2.0 which
+  accompanies this distribution, and is available at
+  https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
+import * as std from 'cm_std';
+import * as xplatform from 'xplatform';
+
+import * as common from '../../../libs/common';
+import * as fs from '../../../libs/fs';
+import * as shell from '../../../libs/shell';
+import * as stringlib from '../../../libs/string';
+
+function requireJava() {
+  const testJava = shell.execSync('sh', '-c', 'java -version 2>/dev/null 1>/dev/null');
+  return testJava.rc;
+}
+
+function processCommResult(content, lines) {
+  let returnedOutput = '';
+  if (content) {
+    let linesSplit = content.split("\n");
+    if (lines) {
+      linesSplit = linesSplit.slice(0, lines);
+    }
+    linesSplit.forEach(line => { 
+      const oneLineSplit = line.split(' ');
+      returnedOutput += `${oneLineSplit[0]}\n` 
+    })
+  }
+  return returnedOutput;
+}
+
+export function execute() {
+
+  common.printLevel0Message('Verify Zowe file fingerprints');
+
+  if (requireJava()) {
+      common.printErrorAndExit('No Java found', undefined, 999);
+  }
+
+  const tmpFilePrefix = 'zwe-support-verify-fingerprints';
+  const zoweRuntime = std.getenv('ZWE_zowe_runtimeDirectory');
+  const manifest = `${zoweRuntime}/manifest.json`
+
+  let manifestContent = undefined;
+  let manifestJson = undefined;
+  if (fs.fileExists(manifest)) {
+      manifestContent = xplatform.loadFileUTF8(manifest, xplatform.AUTO_DETECT);
+  } else {
+      common.printErrorAndExit(`Error ZWEL0150E: Failed to find file "${manifest}". Zowe runtimeDirectory is invalid.`, undefined, 150);
+  }
+  if (manifestContent) {
+      manifestJson = JSON.parse(manifestContent);
+  }
+  if (!manifestContent || !manifestJson.version) {
+      common.printErrorAndExit("Error ZWEL0113E: Failed to find Zowe version. Please validate your Zowe directory.", undefined, 113);
+  }
+  const zoweVersion = manifestJson.version;
+
+  if (!fs.fileExists(`${zoweRuntime}/bin/utils/HashFiles.class`)) {
+      common.printErrorAndExit(`Error ZWEL0150E: Failed to find file "${zoweRuntime}/bin/utils/HashFiles.class". Zowe runtimeDirectory is invalid.`, undefined, 150);
+  }
+
+  if (!fs.fileExists(`${zoweRuntime}/fingerprint/RefRuntimeHash-${zoweVersion}.txt`)) {
+      common.printErrorAndExit(`Error ZWEL0150E: Failed to find file "${zoweRuntime}/fingerprint/RefRuntimeHash-${zoweVersion}.txt". Zowe runtimeDirectory is invalid.`, undefined, 150);
+  }
+
+  common.printMessage('- Create Zowe directory file list');
+  const allFiles = fs.createTmpFile(tmpFilePrefix);
+  shell.execOutSync('sh', '-c', `cd '${zoweRuntime}' && find . -name ./SMPE -prune -o -name "./ZWE*" -prune -o -name ./fingerprint -prune -o -type f -print > "${allFiles}"`);
+  if (!fs.fileExists(allFiles)) {
+    common.printErrorAndExit(`Error ZWEL0151E: Failed to create temporary file "${allFiles}". Please check permission or volume free space.`, undefined, 151);
+  }
+
+  common.printDebug(`  * File list created as ${allFiles}`);
+  shell.execSync('sh', '-c', `chmod 700 "${allFiles}"`);
+
+  common.printMessage('- Calculate hashes of Zowe files');
+
+  const customHashes = fs.createTmpFile(tmpFilePrefix);
+  const javaHash = shell.execOutSync('sh', '-c', `cd '${zoweRuntime}' && java -cp "${zoweRuntime}/bin/utils/" HashFiles "${allFiles}" | sort > "${customHashes}"`);
+
+  if (javaHash.rc != 0 || !fs.fileExists(customHashes) || fs.fileSize(customHashes) < 1) {
+    common.printError(`  * Error ZWEL0151E: Failed to create temporary file ${customHashes}. Please check permission or volume free space.`);
+    common.printError(`  * Exit code: java error code=${javaHash.rc}`)
+    common.printError(`  *            file exists=${fs.fileExists(customHashes)}`);
+    if (fs.fileExists(customHashes)) {
+      common.printError(`  *            file size=${fs.fileSize(customHashes)}`);
+      fs.rmrf(allFiles);
+      fs.rmrf(customHashes);
+    }
+    if (javaHash.out) {
+      common.printError(stringlib.paddingLeft(javaHash.out, "    "));
+    }
+    std.exit(151);
+  }
+
+  common.printDebug(`  * Zowe file hashes created as ${customHashes}`);
+  shell.execSync('sh', '-c', `chmod 700 "${customHashes}"`);
+
+  let verifyFailed = false;
+  const logLevel = std.getenv('ZWE_PRIVATE_LOG_LEVEL_ZWELS');
+  const COMM = [
+    [ 3, 'different' ],
+    [ 13, 'extra' ],
+    [ 23, 'missing' ]
+  ]
+
+  COMM.forEach(commSetting => {
+    const commParameter = commSetting[0];
+    const commStepName = commSetting[1]
+    common.printMessage(`- Find ${commStepName} files`);
+    const commResult = shell.execOutSync('sh', '-c', `cd '${zoweRuntime}' && comm -${commParameter} "${zoweRuntime}/fingerprint/RefRuntimeHash-${zoweVersion}.txt" "${customHashes}"`);
+    if (commResult.rc) {
+      commom.printError(`  * Error ZWEL0151E: Failed to compare hashes of fingerprint/RefRuntimeHash-${zoweVersion}.txt and current.`);
+      commom.printError(`  * Exit code: ${commResult.rc}`);
+      if (commResult.out) {
+        commom.printError(`  * Output:`);
+        common.printError(`${stringlib.paddingLeft(commResult.out, "    ")}`);
+      }
+      fs.rmrf(allFiles);
+      fs.rmrf(customHashes);
+      std.exit(151);
+    }
+
+    if (commResult.out) {
+        const linesReturned =  commResult.out.split("\n").length;
+        common.printMessage(`  * Number of ${commStepName} files: ${linesReturned}`);
+        if (linesReturned) {
+        verifyFailed = true;
+        if (logLevel == 'TRACE' ) {
+            common.printTrace(`  * All ${commStepName} files:`);
+            common.printTrace(processCommResult(commResult.out, undefined));
+        }
+        if (logLevel == 'DEBUG') {
+            common.printDebug(`  * First 10 ${commStepName} files:`);
+            common.printDebug(stringlib.paddingLeft(processCommResult(commResult.out, 10),"    "));
+        }
+        }
+    }
+  });
+  
+  fs.rmrf(allFiles);
+  fs.rmrf(customHashes);
+  
+  if (verifyFailed) {
+    common.printMessage("");
+    common.printErrorAndExit('Error ZWEL0181E: Failed to verify Zowe file fingerprints.', undefined, 151);
+  } else {
+    common.printLevel1Message('Zowe file fingerprints verification passed.');
+  }
+}
