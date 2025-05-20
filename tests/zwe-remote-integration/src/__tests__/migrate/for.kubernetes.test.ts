22ffc1e8283d73822306dee9150b75d2f6229cc1
MarkAckert <mark.ackert@broadcom.com>
2025-05-20 15:41:40 -0400

    basic migrate for kubernetes test case
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

---
+++ tests/zwe-remote-integration/src/__tests__/migrate/for.kubernetes.test.ts
@@ -0,0 +1,54 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+import ZoweYamlType from '../../config/ZoweYamlType';
+import { RemoteTestRunner } from '../../zos/RemoteTestRunner';
+import { ZoweConfig } from '../../config/ZoweConfig';
+import * as path from 'path';
+import { TestFile, TestFileActions } from '../../zos/TestFileActions';
+
+const testSuiteName = 'migrate-for-kubernetes';
+describe(`${testSuiteName}`, () => {
+  let testRunner: RemoteTestRunner;
+  let cfgYaml: ZoweYamlType;
+  const cleanupFiles: TestFile[] = []; // a list of datasets deleted after every test
+  // let defaultCfgYaml: ZoweYamlType;
+  const yamlResourceDir = path.resolve('src', '__tests__', 'migrate', '__resources__');
+
+  beforeAll(async () => {
+    testRunner = new RemoteTestRunner(testSuiteName);
+    cfgYaml = ZoweConfig.getZoweYaml();
+    cfgYaml.zowe.setup.certificate.defaultCfgYaml = ZoweConfig.getDefaultsYaml();
+  });
+  beforeEach(() => {
+    cfgYaml = ZoweConfig.getZoweYaml();
+    // defaultCfgYaml = ZoweConfig.getDefaultsYaml();
+  });
+
+  afterEach(async () => {
+    TestFileActions.deleteAll(cleanupFiles);
+    await testRunner.postTest();
+  });
+
+  afterAll(() => {
+    testRunner.shutdown();
+  });
+
+  describe('(SHORT)', () => {
+    it('run migrate for kubernetes', async () => {
+      const scenarioYml = 'setup.scenario.1.yml';
+      cfgYaml = ZoweConfig.loadAndOverlay(cfgYaml, yamlResourceDir, scenarioYml);
+      let result = await testRunner.runZweTest(cfgYaml, 'init certificate');
+      result = await testRunner.runZweTest(cfgYaml, 'migrate for kubernetes');
+      expect(result.rc).toBe(0);
+      expect(result.cleanedStdout).toMatchSnapshot();
+    });
+  });
+});
