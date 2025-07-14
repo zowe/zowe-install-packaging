f043affd6a13ec1ba256c0a385d714ab4e82984b
MarkAckert <mark.ackert@broadcom.com>
2025-07-14 10:32:15 -0400

    update method name in remote test runner
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- tests/zwe-remote-integration/src/zos/RemoteTestRunner.ts
+++ tests/zwe-remote-integration/src/zos/RemoteTestRunner.ts
@@ -315,14 +315,14 @@ export class RemoteTestRunner {
     opts: { binary: boolean; mode: number } = { binary: true, mode: 0o644 },
   ) {
     const fullRemoteFilePath = `${REMOTE_SYSTEM_INFO.ussTestDir}/${remoteFile}`;
-    await this.removeUssFileForTest(remoteFile); // this tracks/restores
+    await this.removeUssFileOrDirForTest(remoteFile); // this tracks/restores
     await files.Upload.fileToUssFile(this.session, localFile, fullRemoteFilePath, {
       binary: opts.binary,
     });
     await this.runRaw(`chmod ${opts.mode.toString(8)} ${fullRemoteFilePath}`);
   }
 
-  public async removeUssFileForTest(filePath: String) {
+  public async removeUssFileOrDirForTest(filePath: String) {
     const flattenedTmpName = filePath.replaceAll('/', '_');
     await this.runRaw(`mkdir -p ${this.REMOTE_TEST_TMP_DIR}`);
     await this.runRaw(`mv ${filePath} ${this.REMOTE_TEST_TMP_DIR}/${flattenedTmpName}`);
