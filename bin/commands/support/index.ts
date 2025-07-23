3590af57cabce90b15ffc88d23770a596b4942cc
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-07-23 12:53:08 +0200

    Support update
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- bin/commands/support/index.ts
+++ bin/commands/support/index.ts
@@ -193,7 +193,11 @@ export function execute(): void {
   common.printMessage("");
 
   // zowe.job.name + prefix are in defaults
-  const jobName = ZOWE_CONFIG.zowe.job.name;
+  // zowe.job.name could be possibly null/empty
+  let jobName = ZOWE_CONFIG.zowe.job.name;
+  if (!jobName) {
+    jobName = 'ZWESLSTC';
+  }
   const jobPrefix = ZOWE_CONFIG.zowe.job.prefix;
 
   common.printLevel1Message(`Collecting current process information based on the job prefix ${jobPrefix} and job name ${jobName}`);
