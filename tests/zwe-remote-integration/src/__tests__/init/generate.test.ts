c1fb5f8ef24f8afcca291c0eddf2449a3f974636
MarkAckert <mark.ackert@broadcom.com>
2025-07-24 09:55:53 -0400

    update adversarial test to account for longer line lengths and merge
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- tests/zwe-remote-integration/src/__tests__/init/generate.test.ts
+++ tests/zwe-remote-integration/src/__tests__/init/generate.test.ts
@@ -230,9 +230,9 @@ describe(`${testSuiteName}`, () => {
           stdEnvPassed = true;
         }
         if (stdEnvPassed) {
-          expect(line.length <= 71);
-          if (line.length === 71) {
-            expect(line.endsWith(`\\`));
+          expect(line.length <= 73);
+          if (line.length === 73) {
+            expect(line.endsWith(`\\`)).toBe(true);
           }
         }
       });
@@ -241,7 +241,7 @@ describe(`${testSuiteName}`, () => {
     /**
      * This test cannot accurately capture the output of ZWESLSTC as a snapshot, because the
      *  test directory may vary from system to system which changes the output. We check that the correct
-     *  config choices are ordered in the final ZWESLSTC and that 
+     *  config choices are ordered in the final ZWESLSTC and that the lonest lines in STDENV end with backslash
      */
     it('test adversarial paths and concatenations', async () => {
       const MAX_DIR_LEN = 255; // USS restriction
