29022f888b03741d307fefc8447ef3b7a26c1041
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2025-07-31 13:39:48 +0200

    Check length of absolute path
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- bin/commands/init/stc/index.sh
+++ bin/commands/init/stc/index.sh
@@ -137,14 +137,17 @@ else
     print_message "CONFIG path defined in ZWESLSTC is converted into absolute path and may contain SYSNAME."
     print_message "Please manually verify if this path works for your environment, especially when you are working in Sysplex environment."
   fi
-  CFG_LEN_CHECK=$(echo "${ZWE_CLI_PARAMETER_CONFIG}" | awk 'length($0) > 74')
+  configAbsolutePath=$(convert_to_absolute_path "${ZWE_CLI_PARAMETER_CONFIG}")
+  CFG_LEN_CHECK=$(echo "${configAbsolutePath}" | awk 'length($0) > 74')
   if [ -n "${CFG_LEN_CHECK}" ]; then
-    print_error_and_exit "Error ZWEL0129E: The path to the Zowe YAML config must be less than 74 characters long. Config: ${ZWE_CLI_PARAMETER_CONFIG}" "" 129
+    print_error_and_exit "Error ZWEL0129E: The path to the Zowe YAML config must be less than 74 characters. Config: ${configAbsolutePath}" "" 129
   fi
   result=$(cat "//'${prefix}.${ZWE_PRIVATE_DS_SZWESAMP}(ZWESLSTC)'" | \
           sed "s/^\/\/STEPLIB .*\$/\/\/STEPLIB  DD   DSNAME=${authLoadlib},/" | \
-          sed "s#^CONFIG=.*\$#CONFIG=$(convert_to_absolute_path ${ZWE_CLI_PARAMETER_CONFIG})#" \
+          sed "s#^CONFIG=.*\$#CONFIG=${configAbsolutePath}#" \
           > "${tmpfile}")
+  # FIXME: result=$(cat ./FileWhichDoesNotExist | sed "s/a/b/" > /dev/null)
+  #        echo $? -> always 0
   code=$?
   chmod 700 "${tmpfile}"
   if [ ${code} -eq 0 ]; then
