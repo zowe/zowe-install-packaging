b1932d599d6b8a41f697d3e64547ce0ebcc3eaf1
MarkAckert <mark.ackert@broadcom.com>
2025-08-05 14:54:58 -0400

    change error message to 173E
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- bin/libs/certificate.sh
+++ bin/libs/certificate.sh
@@ -848,7 +848,7 @@ keyring_run_zwekring_jcl() {
   done
 
   if [ -z "${ip_address}" ]; then
-    print_error_and_exit "Error ZWEL0156E: Please enter an IP address in either the subject alternative name (zowe.setup.certificate.san) or external domain (zowe.externalDomains) in the Zowe YAML configuration file." "" 156
+    print_error_and_exit "Error ZWEL0173E: Please enter an IP address in either the subject alternative name (zowe.setup.certificate.san) or external domain (zowe.externalDomains) in the Zowe YAML configuration file." "" 173
   fi
 
   import_ext_ca=0
@@ -1554,7 +1554,7 @@ keyring_run_zwekring_jcl_legacy_mode() {
   done
 
   if [ -z "${ip_address}" ]; then
-    print_error_and_exit "Error ZWEL0156E: Please enter an IP address in either the subject alternative name (zowe.setup.certificate.san) or external domain (zowe.externalDomains) in the Zowe YAML configuration file." "" 156
+    print_error_and_exit "Error ZWEL0173E: Please enter an IP address in either the subject alternative name (zowe.setup.certificate.san) or external domain (zowe.externalDomains) in the Zowe YAML configuration file." "" 173
   fi
 
   import_ext_ca=0
