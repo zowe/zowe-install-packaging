6c0f431b689f71d65ba27a51eaf3e31d2d77a981
MarkAckert <mark.ackert@broadcom.com>
2025-09-09 16:11:47 -0400

    re-enable vt tests and editor tests
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- tests/sanity/test/e2e/test-06-vt.js
+++ tests/sanity/test/e2e/test-06-vt.js
@@ -8,7 +8,6 @@
  * Copyright IBM Corporation 2018, 2019
  */
 
-/*
 const path = require('path');
 const expect = require('chai').expect;
 const debug = require('debug')('zowe-sanity-test:e2e:vt');
@@ -104,5 +103,3 @@ describe(`test ${APP_TO_TEST}`, function() {
     }
   });
 });
-
-*/
