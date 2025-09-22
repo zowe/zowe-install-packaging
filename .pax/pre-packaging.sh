9065670ca6703a6c30af0d64be1b21b9fbf39c9f
Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>
2025-09-22 14:53:18 +0200

    Test commit
    Signed-off-by: Martin Zeithaml <66114686+Martin-Zeithaml@users.noreply.github.com>

--- .pax/pre-packaging.sh
+++ .pax/pre-packaging.sh
@@ -240,6 +240,9 @@ cp "${ZOWE_ROOT_DIR}/bin/utils/zowe-server/zowex" "${ZOWE_ROOT_DIR}/bin/utils/zo
 chmod +x "${ZOWE_ROOT_DIR}/bin/utils/zowex"
 cd "${ZOWE_ROOT_DIR}/bin/utils"
 rm -rf "${ZOWE_ROOT_DIR}/bin/utils/zowe-server"
+## TMP
+ls -al "${ZOWE_ROOT_DIR}/files"
+## TMP
 rm "${ZOWE_ROOT_DIR}/files/zowe-server-*.pax.Z"
 
 echo "[$SCRIPT_NAME] change keyring-util to be executable ..."
