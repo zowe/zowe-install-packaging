7996d83dba0babcc111dfc183d744cade792ad27
1000TurquoisePogs <sgrady@rocketsoftware.com>
2025-12-08 13:30:11 +0100

    Refactor and fix parameters
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- bin/commands/components/install/register/zis/loadlib/index.ts
+++ bin/commands/components/install/register/zis/loadlib/index.ts
@@ -10,16 +10,31 @@
 */
 
 import * as std from 'cm_std';
+import * as os from 'cm_os';
 import * as common from '../../../../../../libs/common';
 import * as stringlib from '../../../../../../libs/string';
-import * as shell from '../../../../../../libs/shell';
 import * as config from '../../../../../../libs/config';
 import * as component from '../../../../../../libs/component';
-import * as varlib from '../../../../../../libs/var';
 import { PathAPI as pathoid } from '../../../../../../libs/pathoid';
 
-export function execute(componentName: string, dryRun?: boolean) {
+export function execute(componentName?: string, zisPluginDatasets?: string[], dryRun?: boolean) {
   common.requireZoweYaml();
+
+  if (zisPluginDatasets) {
+    let success = component.addZisLoadLibToStcJcl(zisPluginDatasets, dryRun);
+    if (!success) {
+      common.printErrorAndExit(`ZIS plugin installation failed.`);
+    }
+  } else if (!componentName) {
+    common.printErrorAndExit(`Input component name (-o) or ZIS plugin datasets (-z) required`);
+  } else {
+    
+  }
+
+
+
+
+
   const ZOWE_CONFIG=config.getZoweConfig();
   // read extensionDirectory
   const extensionDir=ZOWE_CONFIG.zowe.extensionDirectory;
@@ -30,10 +45,23 @@ export function execute(componentName: string, dryRun?: boolean) {
   const targetDir = stringlib.removeTrailingSlash(extensionDir);
   const componentDir = pathoid.join(targetDir, componentName);
 
-  component.processZisPluginInstall(componentDir, false, dryRun);
-  /*
-      copyZisPluginToAuthloadlib()
-        or
-      addZisLoadLibToStcJcl()
-   */
+
+  const manifest = component.getManifest(componentDir);
+  if (manifest.zisPlugins) {
+    if (os.platform != 'zos') {
+      common.printErrorAndExit(`ZIS plugin installation must be done on z/OS. Rerun commmand on a Zowe instance on z/OS to complete install`, undefined, 999);
+      
+    } else {
+      if (zisPluginDatasets) {
+        let success = component.addZisLoadLibToStcJcl(zisPluginDatasets, dryRun);
+        if (!success) {
+          common.printErrorAndExit(`ZIS plugin installation failed.`);
+        }
+      } else {
+        component.copyZisPluginsToAuthLoadLib(zisPlugins, dryRun);
+      }
+    }
+  } else {
+    common.printDebug(`Component ${componentName} does not have ZIS plugins, action skipped`);
+  }
 }
